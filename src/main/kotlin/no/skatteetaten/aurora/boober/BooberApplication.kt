8f0135fd20f7e916b63e872d3585ebfd54eb95ea
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-03-10 13:30:38 +0100

    finish setup, use groovy for gradle not kotlin.

--- src/main/kotlin/no/skatteetaten/aurora/boober/BooberApplication.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/BooberApplication.kt
@@ -2,10 +2,20 @@ package no.skatteetaten.aurora.boober
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class BooberApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(BooberApplication::class.java, *args)
+}
+
+@RestController
+class CustomerController  {
+
+    @GetMapping("/")
+    fun hello() = "Hello"
+
 }
