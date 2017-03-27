39a751b03ec3be397f2f3d804cf1c4a3a129585b
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-03-27 21:54:20 +0200

    Some minor refactorings. Maybe controversial.

--- src/main/kotlin/no/skatteetaten/aurora/boober/Configuration.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/Configuration.kt
@@ -4,24 +4,20 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory
+import org.apache.http.impl.client.CloseableHttpClient
 import org.apache.http.impl.client.HttpClients
 import org.apache.http.ssl.SSLContexts
 import org.apache.velocity.app.VelocityEngine
 import org.apache.velocity.runtime.RuntimeConstants
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
-import org.springframework.http.client.ClientHttpRequestFactory
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
 import java.security.cert.X509Certificate
 
 @org.springframework.context.annotation.Configuration
 class Configuration {
-
-    val logger: Logger = LoggerFactory.getLogger(Configuration::class.java)
 
     @Bean
     @Primary
@@ -31,42 +27,38 @@ class Configuration {
 
     @Bean
     fun velocity(): VelocityEngine {
-        val ve  = VelocityEngine()
-        ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath")
-        ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader::class.java.name)
-        ve.init()
-
-        return ve
+        return VelocityEngine().apply {
+            setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath")
+            setProperty("classpath.resource.loader.class", ClasspathResourceLoader::class.java.name)
+            init()
+        }
     }
 
     @Bean
     @Primary
     fun restTemplate(): RestTemplate {
 
-        logger.info("Loading custom rest template")
-        return RestTemplate(clientHttpRequestFactory())
+        fun createSslTrustAllHttpClient(): CloseableHttpClient? {
+            val acceptingTrustStrategy = { chain: Array<X509Certificate>, authType: String -> true }
+
+            val sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build()
+
+            val csf = SSLConnectionSocketFactory(sslContext)
+
+            val httpClient = HttpClients.custom()
+                    .setSSLSocketFactory(csf)
+                    .build()
+            return httpClient
+        }
+
+        val clientHttpRequestFactory = HttpComponentsClientHttpRequestFactory().apply {
+            setReadTimeout(2000)
+            setConnectTimeout(2000)
+            httpClient = createSslTrustAllHttpClient()
+        }
+
+        return RestTemplate(clientHttpRequestFactory)
     }
-
-    private fun clientHttpRequestFactory(): ClientHttpRequestFactory {
-        val factory = HttpComponentsClientHttpRequestFactory()
-        factory.setReadTimeout(2000)
-        factory.setConnectTimeout(2000)
-
-        val acceptingTrustStrategy = { chain: Array<X509Certificate>, authType: String -> true }
-
-        val sslContext = SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build()
-
-        val csf = SSLConnectionSocketFactory(sslContext)
-
-        val httpClient = HttpClients.custom()
-                .setSSLSocketFactory(csf)
-                .build()
-        factory.httpClient = httpClient
-
-        return factory
-    }
-
-
 }
