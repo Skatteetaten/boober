576d0ab6ca043863455a1a2082a298f2a47000d3
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-03-28 20:20:56 +0200

    Refactor

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/AocConfigParserService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/AocConfigParserService.kt
@@ -31,53 +31,21 @@ class AocConfigParserService(
     private fun createAuroraDeploymentConfig(json: JsonNode): AuroraDeploymentConfig {
 
         val type = TemplateType.valueOf(json.s("type") ?: "")
-        val flags = json.l("flags")
-        var name: String? = json.s("name")
+        var name = json.s("name")
 
-        val deploymentDescriptor: Any = when (type) {
+        val deployDescriptor: Any = when (type) {
             TemplateType.process -> {
                 TemplateDeploy()
             }
             else -> {
-                val buildJson: JsonNode = json.get("build")
-                val deployJson: JsonNode = json.get("deploy")
-
-                val artifactId = buildJson.s("ARTIFACT_ID")
-                val groupId = buildJson.s("GROUP_ID")
-
-                name = name ?: artifactId
-
-                var certificateCn = deployJson.s("CERTIFICATE_CN")
-                val generateCertificate = flags.contains("cert") || certificateCn != null
-                if (generateCertificate && certificateCn == null) {
-                    certificateCn = groupId + "." + name
-                }
-
-                val prometheus = if (deployJson.b("PROMETHEUS_ENABLED") ?: true) Prometheus(
-                        deployJson.i("PROMETHEUS_PORT") ?: 8080,
-                        deployJson.s("PROMETHEUS_PATH") ?: "/prometheus"
-                ) else null
-                AuroraDeploy(
-                        artifactId = artifactId!!,
-                        groupId = groupId!!,
-                        version = buildJson.s("VERSION")!!,
-                        splunkIndex = deployJson.s("SPLUNK_INDEX") ?: "",
-                        maxMemory = deployJson.s("MAX_MEMORY") ?: "256Mi",
-                        database = deployJson.s("DATABASE"),
-                        generateCertificate = generateCertificate,
-                        certificateCn = certificateCn!!,
-                        tag = deployJson.s("TAG") ?: "default",
-                        cpuRequest = deployJson.s("CPU_REQUEST") ?: "0",
-                        websealRoute = deployJson.s("ROUTE_WEBSEAL"),
-                        websealRoles = deployJson.s("ROUTE_WEBSEAL_ROLES"),
-                        prometheus = prometheus,
-                        managementPath = deployJson.s("MANAGEMENT_PATH") ?: "",
-                        debug = deployJson.b("DEBUG") ?: false,
-                        alarm = deployJson.b("ALARM") ?: true
-                )
+                val auroraDeploy = createAuroraDeploy(json)
+                // This is kind of messy. Name should probably be required.
+                name = name ?: auroraDeploy.artifactId
+                auroraDeploy
             }
         }
 
+        val flags = json.l("flags")
         val auroraDeploymentConfig = AuroraDeploymentConfig(
                 affiliation = json.s("affiliation") ?: "",
                 cluster = json.s("cluster") ?: "",
@@ -92,10 +60,51 @@ class AocConfigParserService(
                 users = json.s("users") ?: "",
                 route = flags.contains("route"),
                 deploymentStrategy = if (flags.contains("rolling")) rolling else recreate,
-                deployDescriptor = deploymentDescriptor
+                deployDescriptor = deployDescriptor
         )
 
         return auroraDeploymentConfig
+    }
+
+    private fun createAuroraDeploy(json: JsonNode): AuroraDeploy {
+
+        val buildJson: JsonNode = json.get("build")
+        val deployJson: JsonNode = json.get("deploy")
+
+        val artifactId = buildJson.s("ARTIFACT_ID")
+        val groupId = buildJson.s("GROUP_ID")
+
+        var name: String? = json.s("name")
+        name = name ?: artifactId
+
+        var certificateCn = deployJson.s("CERTIFICATE_CN")
+        val generateCertificate = json.l("flags").contains("cert") || certificateCn != null
+        if (generateCertificate && certificateCn == null) {
+            certificateCn = groupId + "." + name
+        }
+
+        val prometheus = if (deployJson.b("PROMETHEUS_ENABLED") ?: true) Prometheus(
+                deployJson.i("PROMETHEUS_PORT") ?: 8080,
+                deployJson.s("PROMETHEUS_PATH") ?: "/prometheus"
+        ) else null
+        return AuroraDeploy(
+                artifactId = artifactId!!,
+                groupId = groupId!!,
+                version = buildJson.s("VERSION")!!,
+                splunkIndex = deployJson.s("SPLUNK_INDEX") ?: "",
+                maxMemory = deployJson.s("MAX_MEMORY") ?: "256Mi",
+                database = deployJson.s("DATABASE"),
+                generateCertificate = generateCertificate,
+                certificateCn = certificateCn!!,
+                tag = deployJson.s("TAG") ?: "default",
+                cpuRequest = deployJson.s("CPU_REQUEST") ?: "0",
+                websealRoute = deployJson.s("ROUTE_WEBSEAL"),
+                websealRoles = deployJson.s("ROUTE_WEBSEAL_ROLES"),
+                prometheus = prometheus,
+                managementPath = deployJson.s("MANAGEMENT_PATH") ?: "",
+                debug = deployJson.b("DEBUG") ?: false,
+                alarm = deployJson.b("ALARM") ?: true
+        )
     }
 }
 
