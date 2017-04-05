6fe2094dfe565a93f38c64e4249ccfb2f5f7c572
Espen Strømland <espen.stromland@skatteetaten.no>
2017-04-05 17:23:26 +0200

    Import alias

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/security/User.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/security/User.kt
@@ -1,7 +1,9 @@
 package no.skatteetaten.aurora.boober.controller.security
 
+import org.springframework.security.core.userdetails.User as SpringSecurityUser
+
 class User(
         username: String,
         val token: String,
         val fullName: String? = null
-) : org.springframework.security.core.userdetails.User(username, token, true, true, true, true, listOf())
+): SpringSecurityUser(username, token, true, true, true, true, listOf())
