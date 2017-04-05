01ec84a82dae15a9dde6c0ea8915a993ed5a4dc5
Espen Strømland <espen.stromland@skatteetaten.no>
2017-04-05 17:22:36 +0200

    Added error message from OpenShift in ErrorHandler

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/ErrorHandler.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/ErrorHandler.kt
@@ -1,6 +1,8 @@
 package no.skatteetaten.aurora.boober.controller
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.skatteetaten.aurora.boober.service.AuroraConfigException
+import no.skatteetaten.aurora.boober.service.OpenShiftException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_REQUEST
@@ -9,6 +11,7 @@ import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
@@ -18,16 +21,20 @@ class ErrorHandler : ResponseEntityExceptionHandler() {
     @ExceptionHandler(AuroraConfigException::class)
     fun handleValidationErrors(ex: AuroraConfigException, req: WebRequest) = handleException(ex, req, BAD_REQUEST)
 
+    @ExceptionHandler(OpenShiftException::class)
+    fun handleOpenShiftErrors(ex: OpenShiftException, req: WebRequest) = handleException(ex, req, BAD_REQUEST)
+
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleBadRequest(ex: IllegalArgumentException, req: WebRequest) = handleException(ex, req, BAD_REQUEST)
 
     @ExceptionHandler(RuntimeException::class)
-    fun handleGenericError(ex: RuntimeException, req: WebRequest) = handleException(ex, req, INTERNAL_SERVER_ERROR)
+    fun handleGenericErrors(ex: RuntimeException, req: WebRequest) = handleException(ex, req, INTERNAL_SERVER_ERROR)
+
 
     private fun handleException(e: Exception, request: WebRequest, httpStatus: HttpStatus): ResponseEntity<*> {
 
         val headers = HttpHeaders().apply { contentType = MediaType.APPLICATION_JSON }
-        val message = "${e.message}. ${e.cause?.message?.let { " Cause: $it" } ?: ""}"
+        val message = createErrorMessage(e)
 
         val items = when (e) {
             is AuroraConfigException -> e.errors
@@ -41,5 +48,19 @@ class ErrorHandler : ResponseEntityExceptionHandler() {
         val response = Response(false, message, items)
 
         return handleExceptionInternal(e, response, headers, httpStatus, request)
+    }
+
+    private fun createErrorMessage(e: Exception): String {
+
+        val cause = e.cause
+        val openShiftMessage = if (cause is HttpClientErrorException) {
+            val json: Map<*, *>? = jacksonObjectMapper().readValue(cause.responseBodyAsString, Map::class.java)
+            if (json?.get("kind")!! == "Status") json["message"] as String? else null
+        } else null
+
+        return StringBuilder().apply {
+            e.message?.let { append(it + ".") }
+            openShiftMessage?.let { append(" " + it) }
+        }.toString()
     }
 }
