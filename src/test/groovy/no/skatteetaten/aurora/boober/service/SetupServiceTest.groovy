e3df781433236cc719332a0ca6540fd920f9a80f
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-04-12 10:17:01 +0200

    first spring boot test for setupService

--- src/test/groovy/no/skatteetaten/aurora/boober/service/SetupServiceTest.groovy
+++ src/test/groovy/no/skatteetaten/aurora/boober/service/SetupServiceTest.groovy
@@ -1,33 +1,62 @@
 package no.skatteetaten.aurora.boober.service
 
-import static no.skatteetaten.aurora.boober.LoggingUtilsKt.setLogLevels
+import static no.skatteetaten.aurora.boober.utils.SampleFilesCollector.getQaEbsUsersSampleFiles
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+import no.skatteetaten.aurora.boober.controller.security.User
 import no.skatteetaten.aurora.boober.controller.security.UserDetailsProvider
+import no.skatteetaten.aurora.boober.model.AuroraConfig
 import spock.lang.Specification
+import spock.mock.DetachedMockFactory
 
+@SpringBootTest(classes = [no.skatteetaten.aurora.boober.Configuration,
+    AuroraConfigParserService,
+    SetupService,
+    OpenShiftService,
+    Config])
 class SetupServiceTest extends Specification {
+
+  @Configuration
+  static class Config {
+    private DetachedMockFactory factory = new DetachedMockFactory()
+
+    @Bean
+    UserDetailsProvider userDetailsProvider() {
+
+      factory.Mock(UserDetailsProvider)
+    }
+
+    @Bean
+    OpenShiftClient openshiftClient() {
+      factory.Mock(OpenShiftClient)
+    }
+  }
+
+  @Autowired
+  SetupService setupService
 
   public static final String ENV_NAME = "booberdev"
   public static final String APP_NAME = "verify-ebs-users"
   final ApplicationId aid = new ApplicationId(ENV_NAME, APP_NAME)
 
-  def parser = new AuroraConfigParserService()
+  def "Should create aurora dc for application"() {
 
-  def udp = Mock(UserDetailsProvider)
-  def openshiftService = new OpenShiftService(udp)
-  def client = Mock(OpenShiftClient)
+    given:
+      setupService.openShiftService.userDetailsProvider.getAuthenticatedUser() >> new User("test", "test", "Test User")
+      setupService.openShiftClient.isValidGroup(_) >> true
+      setupService.openShiftClient.isValidUser(_) >> true
 
-  /*
-  val auroraConfigParserService: AuroraConfigParserService,
-        val openShiftService: OpenShiftService,
-        val openShiftClient: OpenShiftClient
-   */
-  def setupService = SetupService()
+      Map<String, Map<String, Object>> files = getQaEbsUsersSampleFiles()
+      def auroraConfig = new AuroraConfig(files, [:])
 
-  def setupSpec() {
-    setLogLevels()
-  }
+    when:
+      def result = setupService.createAuroraDcForApplication(aid, auroraConfig)
 
-  def "foo"() {
+    then:
+      result != null
   }
 }
