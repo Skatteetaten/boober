099ef6a9bb1b84e1a7c21fff064015c0abea442b
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-04-21 10:42:17 +0200

    Renamed to AuroraConfigController

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/AuroraConfigController.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/AuroraConfigController.kt
@@ -5,12 +5,14 @@ import no.skatteetaten.aurora.boober.controller.SetupCommand
 import no.skatteetaten.aurora.boober.service.GitService
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class GitController(val gitService: GitService) {
+@RequestMapping("/auroraconfig")
+class AuroraConfigController(val gitService: GitService) {
 
-    @PutMapping("/save")
+    @PutMapping("/")
     fun gitSave(@RequestBody cmd: SetupCommand) {
 
         val auroraConfig = AuroraConfig(cmd.files, cmd.secretFiles)
