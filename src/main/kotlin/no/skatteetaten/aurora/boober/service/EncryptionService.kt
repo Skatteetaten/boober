90858e13218e3281b97b44760b1f1b2379368e25
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-04-25 22:52:29 +0200

    Fixed an issue that prevented encryption to work reliably across platforms

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/EncryptionService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/EncryptionService.kt
@@ -1,31 +1,54 @@
 package no.skatteetaten.aurora.boober.service
 
 import org.encryptor4j.Encryptor
+import org.encryptor4j.factory.AbsKeyFactory
 import org.encryptor4j.factory.KeyFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.stereotype.Service
-import java.io.File
+import java.security.Security
 import java.util.*
 
 
+@Configuration
+class EncryptionConfig {
+
+    @Bean
+    fun keyFactory(): KeyFactory = object : AbsKeyFactory("AES", 128) {}
+}
+
 @Service
-class EncryptionService(@Value("\${boober.encrypt.key}") val key: String) {
+class EncryptionService(@Value("\${boober.encrypt.key}") val key: String, keyFactory: KeyFactory) {
+
+    final val providerName: String
+
+    init {
+        val bouncyCastleProvider = org.bouncycastle.jce.provider.BouncyCastleProvider()
+        providerName = bouncyCastleProvider.name
+        Security.getProvider(providerName) ?: Security.addProvider(bouncyCastleProvider)
+    }
 
     val LINE_SEPERATOR = "\n"
+
     val version = "Boober:1"
-    val encryptor = Encryptor(KeyFactory.AES.keyFromPassword(key.toCharArray()), "AES/GCM/PKCS5Padding", 16, 128)
+
+    val encryptor = Encryptor(keyFactory.keyFromPassword(key.toCharArray())).apply {
+        setAlgorithmProvider(providerName)
+    }
 
     fun encrypt(message: String): String {
         val result = encryptor.encrypt(message.toByteArray())
         val encoded = Base64.getEncoder().encodeToString(result)
 
-        val body = "$version$LINE_SEPERATOR$encoded"
-        return body
+        return "$version$LINE_SEPERATOR$encoded"
     }
 
     fun decrypt(source: String): String {
         val split = source.split(LINE_SEPERATOR)
         //If/when we use new versions of encryption here we can use an encryptor for that specific version when we decode.
-        return String(encryptor.decrypt(Base64.getDecoder().decode(split[1])))
+        val cipherTextBase64: String = split[1]
+        val cipherText: ByteArray = Base64.getDecoder().decode(cipherTextBase64)
+        return String(encryptor.decrypt(cipherText))
     }
 }
