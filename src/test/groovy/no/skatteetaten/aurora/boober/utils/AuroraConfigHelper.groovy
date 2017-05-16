5ddcafdad564e2bfcefe3b1b7ef64c43048b64bc
Espen Strømland <espen.stromland@skatteetaten.no>
2017-05-16 14:22:04 +0200

    GitService tests

---
+++ src/test/groovy/no/skatteetaten/aurora/boober/utils/AuroraConfigHelper.groovy
@@ -0,0 +1,15 @@
+package no.skatteetaten.aurora.boober.utils
+
+import com.fasterxml.jackson.databind.JsonNode
+
+import no.skatteetaten.aurora.boober.model.ApplicationId
+import no.skatteetaten.aurora.boober.model.AuroraConfig
+import no.skatteetaten.aurora.boober.model.AuroraConfigFile
+
+class AuroraConfigHelper {
+
+  private static AuroraConfig createAuroraConfig(ApplicationId aid, Map<String, String> secrets = [:]) {
+    Map<String, JsonNode> files = SampleFilesCollector.getSampleFiles(aid)
+    new AuroraConfig(files.collect { new AuroraConfigFile(it.key, it.value, false) }, secrets)
+  }
+}
