e73c6b64d5c7a3408a6ed063e3717f8fd3f12f79
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-05-21 21:04:27 +0200

    restructured the handlers so that they inherit.

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/mapper/v1/AuroraConfigMapperV1.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/mapper/v1/AuroraConfigMapperV1.kt
@@ -5,6 +5,7 @@ import no.skatteetaten.aurora.boober.model.AuroraConfig
 import no.skatteetaten.aurora.boober.model.AuroraConfigFile
 import no.skatteetaten.aurora.boober.service.mapper.AuroraConfigFieldHandler
 import no.skatteetaten.aurora.boober.service.mapper.AuroraConfigMapper
+import no.skatteetaten.aurora.boober.service.mapper.findExtractors
 import no.skatteetaten.aurora.boober.service.openshift.OpenShiftClient
 import no.skatteetaten.aurora.boober.utils.notBlank
 import no.skatteetaten.aurora.boober.utils.pattern
@@ -13,21 +14,20 @@ import no.skatteetaten.aurora.boober.utils.required
 abstract class AuroraConfigMapperV1(aid: ApplicationId, auroraConfig: AuroraConfig, allFiles: List<AuroraConfigFile>, openShiftClient: OpenShiftClient) :
         AuroraConfigMapper(aid, auroraConfig, allFiles, openShiftClient) {
 
-    val baseHandlers = listOf(
+    val v1Handlers = listOf(
             AuroraConfigFieldHandler("schemaVersion", defaultValue = "v1"),
+            AuroraConfigFieldHandler("type", validator = { it.required("Type is required") }),
             AuroraConfigFieldHandler("affiliation", validator = { it.pattern("^[a-z]{0,23}[a-z]$", "Affiliation is must be alphanumeric and under 24 characters") }),
             AuroraConfigFieldHandler("cluster", validator = { it.notBlank("Cluster must be set") }),
-            AuroraConfigFieldHandler("type", validator = { it.required("Type is required") }),
             AuroraConfigFieldHandler("name", validator = { it.pattern("^[a-z][-a-z0-9]{0,23}[a-z0-9]$", "Name must be alphanumeric and under 24 characters") }),
             AuroraConfigFieldHandler("envName"),
             AuroraConfigFieldHandler("flags/route", defaultValue = "false"),
             AuroraConfigFieldHandler("permissions/admin/groups", validator = { it.notBlank("Groups must be set.") }),
             AuroraConfigFieldHandler("permissions/admin/users"),
-            AuroraConfigFieldHandler("splunkIndex"),
             AuroraConfigFieldHandler("database"),
             AuroraConfigFieldHandler("certificateCn"),
             AuroraConfigFieldHandler("webseal/path"),
             AuroraConfigFieldHandler("webseal/roles"),
             AuroraConfigFieldHandler("secretFolder")
-    )
+    ) + allFiles.findExtractors("config")
 }
