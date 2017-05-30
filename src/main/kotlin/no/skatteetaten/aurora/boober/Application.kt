585cc98724dee807631e58e868d804de3214fe2c
Espen Strømland <espen.stromland@skatteetaten.no>
2017-05-30 14:48:20 +0200

    Added Aurora Starters for Spring Boot and Spock. Bumped Spring Boot version and Kotlin version.

--- src/main/kotlin/no/skatteetaten/aurora/boober/Application.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/Application.kt
@@ -2,7 +2,9 @@ package no.skatteetaten.aurora.boober
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import ske.aurora.annotations.AuroraApplication
 
+@AuroraApplication
 @SpringBootApplication
 class Main
 
