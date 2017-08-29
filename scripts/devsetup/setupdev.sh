84149389cd419b156da1b0acd079e7475b60d167
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-08-29 15:02:32 +0200

    Restructured test scripts and created setupdev.sh

---
+++ scripts/devsetup/setupdev.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+FILES_DIR=$SCRIPT_DIR/files
+GIT_FOLDER=/tmp/boobergit/paas
+
+oc whoami -t || (echo "You must be logged into openshift" && exit 1)
+which git || ( echo "Please install git" && sudo apt-get install git)
+
+which http || ( echo "Please install httpie" && sudo apt-get install httpie)
+
+[[ -d $GIT_FOLDER ]] || (mkdir -p $GIT_FOLDER && git init --bare $GIT_FOLDER)
+
+TOKEN=$(oc whoami -t)
+
+echo $TOKEN | sudo tee /opt/boober
+
+echo "Create test Secret Vault"
+http --timeout 300 PUT :8080/affiliation/paas/vault Authorization:"bearer $TOKEN"  < ${FILES_DIR}/secretVault_test.json
+
+echo "Create utv Secret Vault"
+http --timeout 300 PUT :8080/affiliation/paas/vault Authorization:"bearer $TOKEN"  < ${FILES_DIR}/secretVault_utv.json
+
+echo "Add aurora config for referance app"
+http --timeout 300 PUT :8080/affiliation/paas/auroraconfig Authorization:"bearer $TOKEN"  < ${FILES_DIR}/reference.json
+
+echo "The vaults are"
+http --timeout 300 GET :8080/affiliation/paas/vault Authorization:"bearer $TOKEN"
+
+#echo "Dry run deploy of  application to paas-boober-dev and paas-boober-test"
+#http --timeout 300 PUT :8080/affiliation/paas/deploy/dryrun Authorization:"bearer $TOKEN"  < ${FILES_DIR}/deploy.json
