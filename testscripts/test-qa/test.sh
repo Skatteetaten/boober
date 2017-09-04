099c66fef750babc5f7191e28d0e577bed2caa1d
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-09-04 12:34:32 +0200

    remove testscripts

--- testscripts/test-qa/test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-token=$(oc whoami -t)
-cluster=qa
-url=http://boober-aurora.$cluster.paas.skead.no/affiliation/aurora
-
-http --timeout 300 PUT $url/deploy/dryrun Authorization:"bearer $token"  < input.json | jq "."  > res.json
