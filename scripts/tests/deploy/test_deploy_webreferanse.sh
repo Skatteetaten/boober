6260c8b13cf2910262c71e3f826312ea77cfc2b5
Bent André Solheim <BentAndre.Solheim@skatteetaten.no>
2017-09-04 13:58:14 +0200

    Fixed variable name

--- scripts/tests/deploy/test_deploy_webreferanse.sh
+++ scripts/tests/deploy/test_deploy_webreferanse.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FILES_DIR=$SCRIPT_DIR
 
-token=$(oc whoami -t)
+TOKEN=$(oc whoami -t)
 
-http --timeout 300 PUT :8080/affiliation/paas/deploy/dryrun Authorization:"bearer $TOKEN"  < ${FILES_DIR}/deployWebReferanse.json
-http --timeout 300 PUT :8080/affiliation/paas/deploy Authorization:"bearer $token"  < ${FILES_DIR}/deployWebReferanse.json
+#http --timeout 300 PUT :8080/affiliation/paas/deploy/dryrun Authorization:"bearer $TOKEN"  < ${FILES_DIR}/deployWebReferanse.json
+http --timeout 300 PUT :8080/affiliation/paas/deploy Authorization:"bearer $TOKEN"  < ${FILES_DIR}/deployWebReferanse.json
