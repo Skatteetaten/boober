e229c521994564d1915cf1e69229ddff4db9f02a
Espen Strømland <espen.stromland@gmail.com>
2017-09-08 08:48:01 +0200

    Update GitService.kt

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/GitService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/GitService.kt
@@ -43,7 +43,6 @@ class GitService(
             val uri = url.format(affiliation)
 
             try {
-                // TODO: Hvis history er false så skal depth være 1 og shallow true og no tags
                 Git.cloneRepository()
                         .setURI(uri)
                         .setCredentialsProvider(cp)
