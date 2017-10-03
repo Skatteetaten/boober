3e4c7979e867d69ff9c25a4f386c19b197da63e0
Espen Strømland <espen.stromland@gmail.com>
2017-10-03 07:54:39 +0200

    Testing imagestream-import and deployrequest objects (#61)

--- src/test/groovy/no/skatteetaten/aurora/boober/service/DeployServiceGenerateDeployResourceTest.groovy
+++ src/test/groovy/no/skatteetaten/aurora/boober/service/DeployServiceGenerateDeployResourceTest.groovy
@@ -8,14 +8,12 @@ import static no.skatteetaten.aurora.boober.service.openshift.OperationType.UPDA
 
 import org.springframework.beans.factory.annotation.Autowired
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 
 import no.skatteetaten.aurora.boober.model.ApplicationId
 import no.skatteetaten.aurora.boober.service.openshift.OpenShiftClient
 import no.skatteetaten.aurora.boober.service.openshift.OpenShiftResponse
 import no.skatteetaten.aurora.boober.service.openshift.OpenshiftCommand
-import no.skatteetaten.aurora.boober.service.openshift.OperationType
 
 class DeployServiceGenerateDeployResourceTest extends AbstractMockedOpenShiftSpecification {
 
@@ -33,7 +31,7 @@ class DeployServiceGenerateDeployResourceTest extends AbstractMockedOpenShiftSpe
   final ApplicationId aid = new ApplicationId(ENV_NAME, APP_NAME)
 
   def setup() {
-    openShiftClient.prepare(_, _) >> { new OpenshiftCommand(OperationType.CREATE, it[1]) }
+    openShiftClient.prepare(_, _) >> { new OpenshiftCommand(CREATE, it[1]) }
     openShiftClient.performOpenShiftCommand(_, _) >> {
       OpenshiftCommand cmd = it[1]
       new OpenShiftResponse(cmd, cmd.payload)
@@ -41,16 +39,6 @@ class DeployServiceGenerateDeployResourceTest extends AbstractMockedOpenShiftSpe
   }
 
   def docker = "docker/foo/bar:baz"
-
-  def createOpenShiftResponse(String kind, OperationType operationType, int prevVersion, int currVersion) {
-    def previous = mapper.
-        convertValue(["kind": kind, "metadata": ["labels": ["releasedVersion": prevVersion]]], JsonNode.class)
-    def payload = Mock(JsonNode)
-    def response = mapper.
-        convertValue(["kind": kind, "metadata": ["labels": ["releasedVersion": currVersion]]], JsonNode.class)
-
-    return new OpenShiftResponse(new OpenshiftCommand(operationType, payload, previous, null), response)
-  }
 
   def "Should  not create any resource on build flow"() {
     given:
@@ -116,7 +104,7 @@ class DeployServiceGenerateDeployResourceTest extends AbstractMockedOpenShiftSpe
     given:
       def templateType = deploy
       def name = "boober"
-      def imagestream = createOpenShiftResponse("imagestream", OperationType.CREATE, 1, 1)
+      def imagestream = createOpenShiftResponse("imagestream", CREATE, 1, 1)
 
     when:
       def result = deployService.generateRedeployResource([imagestream], templateType, name, docker, true)
