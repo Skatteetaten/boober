9d2d0766331e04844de430023b6bb96d2dc2fddc
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-10-13 21:34:14 +0200

    fix tests after merges.

--- src/test/groovy/no/skatteetaten/aurora/boober/service/AuroraDeploymentConfigDeployServiceTest.groovy
+++ src/test/groovy/no/skatteetaten/aurora/boober/service/AuroraDeploymentConfigDeployServiceTest.groovy
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired
 
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.node.ObjectNode
 
 import no.skatteetaten.aurora.boober.model.ApplicationId
 import no.skatteetaten.aurora.boober.model.AuroraConfig
