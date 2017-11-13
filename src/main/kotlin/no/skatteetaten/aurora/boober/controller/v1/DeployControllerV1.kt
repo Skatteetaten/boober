08d52d3264a63a0f59075c898d3523fab895ed1c
Espen Strømland <espen.stromland@skatteetaten.no>
2017-11-13 16:07:04 +0100

    updated test files and added affiliation to apply endpoint

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/v1/DeployControllerV1.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/v1/DeployControllerV1.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/apply")
+@RequestMapping("/v1/apply/{affiliation}")
 class DeployControllerV1(val deployService: DeployService) {
 
     @PutMapping()
