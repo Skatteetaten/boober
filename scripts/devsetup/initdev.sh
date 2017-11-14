9dec171ff7d11f0b216f46f37b88062e00ac300f
Espen Strømland <espen.stromland@skatteetaten.no>
2017-11-14 13:59:34 +0100

    updated test scripts with v1 api

--- scripts/devsetup/initdev.sh
+++ scripts/devsetup/initdev.sh
@@ -2,5 +2,11 @@
 
 which git || ( echo "Please install git" && sudo apt-get install git)
 
+
 GIT_FOLDER=/tmp/boobergit/paas
+BOOBER_CHECKOUT_FOLDER=/tmp/boober/paas
+
+rm -rf $GIT_FOLDER
+rm -rf $BOOBER_CHECKOUT_FOLDER
+
 [[ -d $GIT_FOLDER ]] || (mkdir -p $GIT_FOLDER && git init --bare $GIT_FOLDER)
