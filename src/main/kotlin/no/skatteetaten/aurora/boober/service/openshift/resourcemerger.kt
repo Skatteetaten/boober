a8b99525da2de1352b91c96895c60f54ede786b5
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2017-11-17 16:10:48 +0100

    Refactorings

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/openshift/resourcemerger.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/openshift/resourcemerger.kt
@@ -5,6 +5,11 @@ import com.fasterxml.jackson.databind.node.ArrayNode
 import no.skatteetaten.aurora.boober.utils.openshiftKind
 import no.skatteetaten.aurora.boober.utils.updateField
 
+/**
+ * When generating OpenShift objects that is an update or a replacement for existing object, we sometimes need to
+ * preserve some information from the existing objects that has been set by OpenShift. This method will make sure that
+ * the required information is preserved.
+ */
 fun mergeWithExistingResource(newResource: JsonNode, existingResource: JsonNode): JsonNode {
 
     val mergedResource = newResource.deepCopy<JsonNode>()
@@ -13,28 +18,34 @@ fun mergeWithExistingResource(newResource: JsonNode, existingResource: JsonNode)
 
     mergedResource.updateField(existingResource, "/metadata", "resourceVersion")
 
-    if (kind == "service") {
-        mergedResource.updateField(existingResource, "/spec", "clusterIP")
+    when (kind) {
+        "service" -> updateService(mergedResource, existingResource)
+        "persistentvolumeclaim" -> updatePersistentVolumeClaim(mergedResource, existingResource)
+        "deploymentconfig" -> updateDeploymentConfig(mergedResource, existingResource)
+        "buildconfig" -> updateBuildConfig(mergedResource, existingResource)
     }
-
-    if (kind == "persistentvolumeclaim") {
-        mergedResource.updateField(existingResource, "/spec", "volumeName")
-    }
-
-    if (kind == "deploymentconfig") {
-        mergedResource.updateField(existingResource, "/spec/triggers/0/imageChangeParams", "lastTriggeredImage")
-        val containerCount = (mergedResource.at("/spec/template/spec/containers") as ArrayNode).size()
-        (0..containerCount).forEach {
-            mergedResource.updateField(existingResource, "/spec/template/spec/containers/$it", "image")
-        }
-    }
-
-    if (kind == "buildconfig") {
-        val triggerCount = (mergedResource.at("/spec/triggers") as ArrayNode).size()
-        (0..triggerCount).forEach {
-            mergedResource.updateField(existingResource, "/spec/triggers/$it/imageChange", "lastTriggeredImageID")
-        }
-    }
-
     return mergedResource
+}
+
+private fun updateBuildConfig(mergedResource: JsonNode, existingResource: JsonNode) {
+    val triggerCount = (mergedResource.at("/spec/triggers") as ArrayNode).size()
+    (0..triggerCount).forEach {
+        mergedResource.updateField(existingResource, "/spec/triggers/$it/imageChange", "lastTriggeredImageID")
+    }
+}
+
+private fun updateDeploymentConfig(mergedResource: JsonNode, existingResource: JsonNode) {
+    mergedResource.updateField(existingResource, "/spec/triggers/0/imageChangeParams", "lastTriggeredImage")
+    val containerCount = (mergedResource.at("/spec/template/spec/containers") as ArrayNode).size()
+    (0..containerCount).forEach {
+        mergedResource.updateField(existingResource, "/spec/template/spec/containers/$it", "image")
+    }
+}
+
+private fun updatePersistentVolumeClaim(mergedResource: JsonNode, existingResource: JsonNode) {
+    mergedResource.updateField(existingResource, "/spec", "volumeName")
+}
+
+private fun updateService(mergedResource: JsonNode, existingResource: JsonNode) {
+    mergedResource.updateField(existingResource, "/spec", "clusterIP")
 }
