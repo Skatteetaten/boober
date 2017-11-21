740b697db5a1d391e71b34206e7adc15db676ef4
Bjarte Stien Karlsen <Bjarte.Karlsen@skatteetaten.no>
2017-11-21 15:17:54 +0100

    renamed service and parameterized where we store auroraConfig

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/v1/InformationControllerV1.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/v1/InformationControllerV1.kt
@@ -1,13 +1,13 @@
 package no.skatteetaten.aurora.boober.controller.v1
 
 import no.skatteetaten.aurora.boober.controller.internal.Response
-import no.skatteetaten.aurora.boober.service.BitbucketService
+import no.skatteetaten.aurora.boober.service.BitbucketProjectService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class InformationControllerV1(val bitbucketService: BitbucketService) {
+class InformationControllerV1(val bitbucketProjectService: BitbucketProjectService) {
 
     @GetMapping("/v1/auroraconfig")
-    fun auroraConfigs(): Response = Response(items = bitbucketService.auroraConfigs())
+    fun auroraConfigs(): Response = Response(items = bitbucketProjectService.getAllSlugs())
 }
