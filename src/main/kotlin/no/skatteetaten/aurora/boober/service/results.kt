91e577c0a57a2d747d59737d5eb334b2757edad9
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-01-17 10:57:56 +0100

    added better error messages to DeployResult

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/results.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/results.kt
@@ -1,26 +1,23 @@
 package no.skatteetaten.aurora.boober.service
 
 import com.fasterxml.jackson.databind.JsonNode
-import no.skatteetaten.aurora.boober.model.ApplicationId
-import no.skatteetaten.aurora.boober.model.AuroraDeployEnvironment
 import no.skatteetaten.aurora.boober.model.AuroraDeploymentSpec
 import no.skatteetaten.aurora.boober.service.openshift.OpenShiftResponse
 import org.eclipse.jgit.lib.PersonIdent
-
-data class AuroraEnvironmentResult(
-        val openShiftResponses: List<OpenShiftResponse> = listOf(),
-        val newNamespace: Boolean
-)
+import java.util.*
 
 
 data class AuroraDeployResult @JvmOverloads constructor(
-        val deployId: String,
-        val auroraDeploymentSpec: AuroraDeploymentSpec,
+        val auroraDeploymentSpec: AuroraDeploymentSpec? = null,
+        val deployId: String= UUID.randomUUID().toString(),
         val openShiftResponses: List<OpenShiftResponse> = listOf(),
         val success: Boolean = true,
         val reason: String? = null,
-        val tagResponse: TagResult? = null){
-    val tag: String = "${auroraDeploymentSpec.cluster}.${auroraDeploymentSpec.environment.namespace}.${auroraDeploymentSpec.name}/${deployId}"
+        val tagResponse: TagResult? = null,
+        val projectExist:Boolean=false){
+    val tag: String = "${auroraDeploymentSpec?.cluster}.${auroraDeploymentSpec?.environment?.namespace}.${auroraDeploymentSpec?.name}/${deployId}"
 }
+
+
 
 data class DeployHistory(val ident: PersonIdent, val result: JsonNode)
