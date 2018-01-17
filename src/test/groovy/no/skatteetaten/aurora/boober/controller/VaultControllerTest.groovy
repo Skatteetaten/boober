1b91315c5705719c903a80c5e5c828ef97e07a61
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2018-01-17 10:23:04 +0100

    Fixed issue with vault contents being base64 encoded

--- src/test/groovy/no/skatteetaten/aurora/boober/controller/VaultControllerTest.groovy
+++ src/test/groovy/no/skatteetaten/aurora/boober/controller/VaultControllerTest.groovy
@@ -2,6 +2,7 @@ package no.skatteetaten.aurora.boober.controller
 
 import static org.springframework.http.MediaType.APPLICATION_JSON
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
@@ -12,8 +13,10 @@ import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
 import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
 import no.skatteetaten.aurora.boober.controller.internal.ErrorHandler
 import no.skatteetaten.aurora.boober.controller.v1.VaultControllerV1
+import no.skatteetaten.aurora.boober.service.FolderHelperKt
 import no.skatteetaten.aurora.boober.service.vault.EncryptedFileVault
 import no.skatteetaten.aurora.boober.service.vault.VaultService
 import spock.lang.Specification
@@ -42,7 +45,8 @@ class VaultControllerTest extends Specification {
 
     given:
       def payload = [name: 'testVault', secrets: [:], permissions: []]
-      1 * vaultService.import(vaultCollectionName, 'testVault', [], [:]) >> EncryptedFileVault.createFromFolder(new File("."))
+      1 * vaultService.import(vaultCollectionName, 'testVault', [], [:]) >>
+          EncryptedFileVault.createFromFolder(new File("."))
 
     when:
       ResultActions result = mockMvc.perform(
@@ -53,14 +57,13 @@ class VaultControllerTest extends Specification {
       result.andExpect(status().isOk())
   }
 
-  def "Simple test that verifies payload is correctly parsed on update secret file"() {
+  def "Fails when provided secret file payload is not Base64 encoded"() {
 
     given:
       def vaultName = "some_vault"
       def secretName = "some_secret"
       def fileContents = 'SECRET_PASS=asdlfkjaølfjaøf'
-      def payload = [contents: fileContents, validateVersions: false]
-      1 * vaultService.createOrUpdateFileInVault(vaultCollectionName, vaultName, secretName, fileContents) >> EncryptedFileVault.createFromFolder(new File("."))
+      def payload = [contents: fileContents]
 
     when:
       ResultActions result = mockMvc.perform(
@@ -70,5 +73,53 @@ class VaultControllerTest extends Specification {
 
     then:
       result.andExpect(status().isOk())
+  }
+
+  def "Succeeds when provided secret file payload is Base64 encoded"() {
+
+    given:
+      def vaultName = "some_vault"
+      def secretName = "some_secret"
+      def fileContents = 'SECRET_PASS=asdlfkjaølfjaøf'
+      def payload = [contents: fileContents.bytes.encodeBase64().toString()]
+      1 * vaultService.createOrUpdateFileInVault(vaultCollectionName, vaultName, secretName, fileContents.bytes) >>
+          EncryptedFileVault.createFromFolder(new File("."))
+
+    when:
+      ResultActions result = mockMvc.perform(
+          put("/v1/vault/$vaultCollectionName/$vaultName/$secretName")
+              .content(JsonOutput.toJson(payload))
+              .contentType(APPLICATION_JSON))
+
+    then:
+      result.andExpect(status().isOk())
+  }
+
+  def "Get vault returns Base64 encoded content"() {
+
+    given:
+      def vaultName = "some_vault"
+      def secretName = "some_secret"
+      def fileContents = 'SECRET_PASS=asdlfkjaølfjaøf'
+      EncryptedFileVault vault = createTestVault(vaultName, secretName, fileContents)
+      1 * vaultService.findVault(vaultCollectionName, vaultName) >> vault
+
+    when:
+      ResultActions result = mockMvc.perform(get("/v1/vault/$vaultCollectionName/$vaultName"))
+
+    then:
+      result.andExpect(status().isOk())
+      def resultBody = new JsonSlurper().parseText(result.andReturn().response.contentAsString)
+      def vaultResponse = resultBody.items[0]
+      vaultResponse.name == vaultName
+      vaultResponse.secrets.size() == 1
+      vaultResponse.secrets.some_secret == fileContents.bytes.encodeBase64().toString()
+  }
+
+  private EncryptedFileVault createTestVault(String vaultName, String secretName, String fileContents) {
+    def folder = FolderHelperKt.recreateFolder("build/vaults/$vaultCollectionName/$vaultName")
+    new File(folder, secretName).write(fileContents)
+    def vault = EncryptedFileVault.createFromFolder(folder)
+    vault
   }
 }
---
+++ src/test/kotlin/no/skatteetaten/aurora/boober/service/FolderHelper.kt
@@ -0,0 +1,14 @@
+package no.skatteetaten.aurora.boober.service
+
+import java.io.File
+
+
+fun recreateFolder(folder: String) = recreateFolder(File(folder))
+
+fun recreateFolder(folder: File): File {
+    if (folder.exists()) {
+        folder.deleteRecursively()
+    }
+    folder.mkdirs()
+    return folder
+}
