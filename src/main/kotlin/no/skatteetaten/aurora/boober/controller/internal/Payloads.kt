226bb32a4fe4c0466831df00b5b14e6ac00bcc64
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2018-02-01 15:03:41 +0100

    remove JsonDataFiles alias

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/internal/Payloads.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/internal/Payloads.kt
@@ -3,11 +3,10 @@ package no.skatteetaten.aurora.boober.controller.internal
 import no.skatteetaten.aurora.boober.model.ApplicationId
 import no.skatteetaten.aurora.boober.model.AuroraConfigFile
 
-typealias JsonDataFiles = Map<String, String>
 
 
 data class ApplyPayload(val applicationIds: List<ApplicationId>,
-                        val overrides: JsonDataFiles = mapOf(),
+                        val overrides: Map<String, String> = mapOf(),
                         val deploy: Boolean = true
 ) {
     fun overridesToAuroraConfigFiles(): List<AuroraConfigFile> {
