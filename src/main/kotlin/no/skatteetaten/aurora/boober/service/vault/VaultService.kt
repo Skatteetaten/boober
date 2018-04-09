5ed608a41dcea010e823d3c9fb099303c5013ef2
Bjarte S. Karlsen <bjarte@bjartek.org>
2018-04-09 10:53:16 +0200

    AOS-2524 validere secret vault keys (#203)
    * validate secret properties keys.
    
    * validate secret properties keys.
    
    can not load as properties file since space is a delimiter
    
    * remove escape of whitespace
    
    * Moved assertSecretKeysAreValid into companion object and added some test

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/vault/VaultService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/vault/VaultService.kt
@@ -66,6 +66,7 @@ class VaultService(
                                   previousSignature: String? = null
     ): EncryptedFileVault {
 
+        assertSecretKeysAreValid(mapOf(fileName to fileContents))
         return withVaultCollectionAndRepoForUpdate(vaultCollectionName, { vaultCollection, repo ->
             val vault = findVaultByNameIfAllowed(vaultCollection, vaultName) ?: vaultCollection.createVault(vaultName)
 
@@ -115,6 +116,8 @@ class VaultService(
     fun import(vaultCollectionName: String, vaultName: String, permissions: List<String>, secrets: Map<String, ByteArray>): EncryptedFileVault {
 
         assertCurrentUserHasAccess(permissions)
+
+        assertSecretKeysAreValid(secrets)
         return withVaultCollectionAndRepoForUpdate(vaultCollectionName, { vaultCollection, repo ->
             vaultCollection.createVault(vaultName).let { vault ->
                 vault.clear()
@@ -146,7 +149,7 @@ class VaultService(
         val user = userDetailsProvider.getAuthenticatedUser()
         if (!user.hasAnyRole(permissions)) {
             val message = "You (${user.username}) do not have required permissions ($permissions) to " +
-                "operate on this vault. You have ${user.authorities.map { it.authority }}"
+                    "operate on this vault. You have ${user.authorities.map { it.authority }}"
             throw UnauthorizedAccessException(message)
         }
     }
@@ -172,5 +175,30 @@ class VaultService(
     private fun findAllVaultsInVaultCollection(vaultCollectionName: String): List<EncryptedFileVault> {
 
         return findVaultCollection(vaultCollectionName).vaults
+    }
+
+    companion object {
+        val rePattern = "^[-._a-zA-Z0-9]+$"
+        val re = Regex(rePattern)
+
+        //Note that a properties file can be delimitered by space and =, very few people know this so we check for it
+        @JvmStatic
+        protected fun assertSecretKeysAreValid(secrets: Map<String, ByteArray>) {
+            val filesToKeys = secrets.filter { it.key.endsWith(".properties") }
+                    .mapValues {
+                        String(it.value)
+                                .lines()
+                                .map { it.substringBefore("=") }
+                                .filter { !it.matches(re) }
+                    }
+
+            val invalidKeys = filesToKeys.flatMap { fileToKey ->
+                fileToKey.value.map { "${fileToKey.key}/$it" }
+            }
+
+            if (invalidKeys.isNotEmpty()) {
+                throw IllegalArgumentException("Vault key=${invalidKeys} is not valid. Regex used for matching $rePattern")
+            }
+        }
     }
 }
