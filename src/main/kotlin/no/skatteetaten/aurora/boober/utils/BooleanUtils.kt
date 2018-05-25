210ed1cbe795fabc85b2d60f1fc391eaf2a18330
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-05-25 08:25:42 +0200

    fix boolean utils logic

--- src/main/kotlin/no/skatteetaten/aurora/boober/utils/BooleanUtils.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/utils/BooleanUtils.kt
@@ -1,4 +1,4 @@
 package no.skatteetaten.aurora.boober.utils
 
-fun <T> Boolean.whenTrue(fn: () -> T?): T? = this.takeIf { this }.let { fn() }
-fun <T> Boolean.whenFalse(fn: () -> T?): T? = this.takeUnless { this }.let { fn() }
+fun <T> Boolean.whenTrue(fn: () -> T?): T? = this.takeIf { this }?.let { fn() }
+fun <T> Boolean.whenFalse(fn: () -> T?): T? = this.takeUnless { this }?.let { fn() }
