eee8817babf164a2c7d5fbf92bea0da83b78f525
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-08-24 10:32:08 +0200

    fix keystore loading issue on ocp

--- src/main/kotlin/no/skatteetaten/aurora/boober/Configuration.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/Configuration.kt
@@ -13,7 +13,6 @@ import org.encryptor4j.factory.KeyFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.beans.factory.config.BeanPostProcessor
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -131,7 +130,7 @@ class Configuration : BeanPostProcessor {
         return build
     }
 
-    @ConditionalOnMissingBean(KeyStore::class)
+    @Profile("local")
     @Bean
     fun localKeyStore(): KeyStore? = null
 
