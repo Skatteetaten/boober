caf58b319f6229bb3db7cb3c93e90da431e0891a
Bjarte Stien Karlsen <bjarte.karlsen@skatteetate.no>
2018-08-24 13:37:04 +0200

    fix deploy from ao with old applications

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/internal/ApplyPayload.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/internal/ApplyPayload.kt
@@ -1,14 +1,22 @@
 package no.skatteetaten.aurora.boober.controller.internal
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import no.skatteetaten.aurora.boober.model.ApplicationDeploymentRef
 import no.skatteetaten.aurora.boober.model.AuroraConfigFile
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 data class ApplyPayload(
-    val applicationDeploymentRefs: List<ApplicationDeploymentRef>,
+    val applicationDeploymentRefs: List<ApplicationDeploymentRef> = emptyList(),
+    val applicationIds: List<ApplicationDeploymentRef> = emptyList(),
     val overrides: Map<String, String> = mapOf(),
     val deploy: Boolean = true
 ) {
     fun overridesToAuroraConfigFiles(): List<AuroraConfigFile> {
         return overrides.map { AuroraConfigFile(it.key, it.value, true) }
     }
+
+    val allRefs: List<ApplicationDeploymentRef>
+        get() = applicationDeploymentRefs + applicationIds
 }
