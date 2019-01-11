cf402ce48fde19fc8fca2bf3f0d3a7a8ec059003
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-01-11 13:46:01 +0100

    add ownerReference to payload from gilis

--- src/main/kotlin/no/skatteetaten/aurora/boober/service/StsRenewService.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/StsRenewService.kt
@@ -3,6 +3,9 @@ package no.skatteetaten.aurora.boober.service
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.module.kotlin.convertValue
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fkorotkov.kubernetes.newOwnerReference
+import io.fabric8.kubernetes.api.model.OwnerReference
+import io.fabric8.kubernetes.api.model.OwnerReferenceBuilder
 import no.skatteetaten.aurora.boober.service.internal.StsSecretGenerator
 import no.skatteetaten.aurora.boober.service.openshift.OpenShiftClient
 import no.skatteetaten.aurora.boober.service.openshift.OpenShiftResponse
@@ -15,12 +18,21 @@ data class RenewRequest(
     val name: String,
     val namespace: String,
     val affiliation: String,
-    val commonName: String
+    val commonName: String,
+    val owner: Owner
 )
+
+data class Owner(
+    val apiVersion: String,
+    val kind: String,
+    val name: String,
+    val uid: String
+)
+
 @Service
 class StsRenewService(
     val provsioner: StsProvisioner,
-    val commandBuilder: OpenShiftCommandBuilder,
+    val commandService: OpenShiftCommandService,
     val openshiftClient: OpenShiftClient,
     val redeployer: RedeployService,
     val userDetailsProvider: UserDetailsProvider
@@ -34,16 +46,22 @@ class StsRenewService(
             request.namespace
         )
 
+        val ownerReference = OwnerReferenceBuilder()
+            .withApiVersion(request.owner.apiVersion)
+            .withKind(request.owner.kind)
+            .withName(request.owner.name)
+            .withUid(request.owner.uid)
+            .build()
+
         val labels = mapOf(
             "app" to request.name,
             "updatedBy" to userDetailsProvider.getAuthenticatedUser().username.replace(":", "-"),
             "affiliation" to request.affiliation
         )
 
-        // TODO: Need to fetch owner reference here, or get it in the RenewRequest.
         val secret = StsSecretGenerator.create(request.name, stsResult, labels, ownerReference)
         val json = jacksonObjectMapper().convertValue<JsonNode>(secret)
-        val command = commandBuilder.createOpenShiftCommand(request.namespace, json)
+        val command = commandService.createOpenShiftCommand(request.namespace, json)
 
         val response = openshiftClient.performOpenShiftCommand(request.namespace, command)
         val deployResult = response.success.whenTrue {
