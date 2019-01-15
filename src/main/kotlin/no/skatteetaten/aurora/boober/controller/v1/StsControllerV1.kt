674e2a8c9aeb3f94ff94fdcc98af2c405b392375
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2019-01-15 08:51:07 +0100

    added better error messages if things fail, and a better message if it succeeds

--- src/main/kotlin/no/skatteetaten/aurora/boober/controller/v1/StsControllerV1.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/v1/StsControllerV1.kt
@@ -3,6 +3,8 @@ package no.skatteetaten.aurora.boober.controller.v1
 import no.skatteetaten.aurora.boober.controller.internal.Response
 import no.skatteetaten.aurora.boober.service.RenewRequest
 import no.skatteetaten.aurora.boober.service.StsRenewService
+import no.skatteetaten.aurora.boober.utils.openshiftKind
+import no.skatteetaten.aurora.boober.utils.openshiftName
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,8 +19,20 @@ class StsControllerV1(val service: StsRenewService) {
 
         val items = service.renew(payload)
 
+        val failed = items.first { !it.success }
+        failed.let {
+            val cmd = it.command.payload
+            return Response(
+                items = items,
+                success = false,
+                message = failed.exception
+                    ?: "Failed running command kind=${cmd.openshiftKind} name=${cmd.openshiftName}"
+            )
+        }
         return Response(
-            items = items
+            items = items,
+            success = true,
+            message = "Renewd cert for affiliaion=${payload.affiliation} namespace=${payload.namespace} name=${payload.name} with commonName=${payload.commonName}"
         )
     }
 }
