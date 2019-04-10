fe9953bfce3772a2118f777dad455aacb499d657
Kristoffer Moberg Christensen <kristofferMoberg.christensen@skatteetaten.no>
2019-04-10 14:43:54 +0200

    ApplicationDeploymentControllerTest that will be implemented in spring cleaning branch

---
+++ src/test/kotlin/no/skatteetaten/aurora/boober/controller/v1/ApplicationDeploymentControllerTest.kt
@@ -0,0 +1,63 @@
+package no.skatteetaten.aurora.boober.controller.v1
+/*
+import com.nhaarman.mockito_kotlin.any
+import no.skatteetaten.aurora.boober.model.ApplicationDeploymentRef
+import no.skatteetaten.aurora.boober.service.DeleteApplicationDeploymentResponse
+import no.skatteetaten.aurora.mockmvc.extensions.Path
+import no.skatteetaten.aurora.mockmvc.extensions.mock.withContractResponse
+import no.skatteetaten.aurora.mockmvc.extensions.post
+import no.skatteetaten.aurora.mockmvc.extensions.responseJsonPath
+import no.skatteetaten.aurora.mockmvc.extensions.statusIsOk
+import org.junit.Ignore
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.mockito.BDDMockito.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.servlet.MockMvc
+*/
+/*@Disabled("TODO: implement this after springcleaning")
+@AutoConfigureRestDocs
+@WebMvcTest(
+    value = [ApplicationDeploymentController::class, ApplicationDeploymentResponder::class],
+    secure = false
+)
+class ApplicationDeploymentControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @MockBean
+    private lateinit var applicationDeploymentDeleteResponder: ApplicationDeploymentResponder
+
+    @Ignore
+    @Test
+    fun `delete ApplicationDeployment given two ApplicationDeploymentRef return success`() {
+        val path = "/v1/ApplicationDeploymentdelete"
+
+        val applicationDeploymentRef = ApplicationDeploymentRef("demo", "test")
+        val applicationDeploymentRefs = listOf(applicationDeploymentRef, applicationDeploymentRef)
+        val deletePayload = DeletePayload(applicationDeploymentRefs)
+
+        val applicationDeploymentDeleteResponse =
+            DeleteApplicationDeploymentResponse(applicationDeploymentRef, true, "success")
+        val applicationDeploymentDeleteResponses =
+            listOf(applicationDeploymentDeleteResponse, applicationDeploymentDeleteResponse)
+
+        // given(applicationDeploymentService.executeDelete(any())).willReturn(applicationDeploymentDeleteResponses)
+
+        val deleteResponse =
+            given(applicationDeploymentDeleteResponder.create(any()))
+                .withContractResponse("applicationdeploymentdelete/success") { willReturn(content) }.mockResponse
+
+        mockMvc.post(
+            path = Path(path),
+            body = deletePayload
+        ) {
+            statusIsOk()
+                .responseJsonPath("$").equalsObject(deleteResponse)
+        }
+    }
+}*/
