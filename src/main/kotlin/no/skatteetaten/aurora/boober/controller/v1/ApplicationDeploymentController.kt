bbcec11b91f1b074a7fcb5e94cd54d535d1ca883
Kristoffer Moberg Christensen <kristofferMoberg.christensen@skatteetaten.no>
2019-04-10 14:41:15 +0200

    Implementation of ApplicationDeploymentController and ApplicationDeploymentService

---
+++ src/main/kotlin/no/skatteetaten/aurora/boober/controller/v1/ApplicationDeploymentController.kt
@@ -0,0 +1,85 @@
+package no.skatteetaten.aurora.boober.controller.v1
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import no.skatteetaten.aurora.boober.controller.internal.Response
+import no.skatteetaten.aurora.boober.service.ApplicationDeploymentService
+import no.skatteetaten.aurora.boober.service.AuroraConfigService
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+data class ApplicationDeploymentPayload(val applicationRefs: List<ApplicationRef>)
+data class ApplicationRef(val namespace: String, val name: String)
+
+@RestController
+@RequestMapping("/v1/applicationdeployment")
+class ApplicationDeploymentController(
+    val applicationDeploymentService: ApplicationDeploymentService,
+    val applicationDeploymentDeleteResponder: ApplicationDeploymentResponder,
+    val auroraConfigService: AuroraConfigService
+) {
+
+    @PostMapping("/delete")
+    fun delete(@RequestBody applicationDeploymentPayload: ApplicationDeploymentPayload): Response {
+
+        val applicationDeploymentDeleteResponse =
+            applicationDeploymentService.executeDelete(applicationDeploymentPayload.applicationRefs)
+
+        val deleteResponses = applicationDeploymentDeleteResponse.map {
+            DeleteResponse(applicationRef = it.applicationRef, success = it.success, reason = it.message)
+        }
+
+        return applicationDeploymentDeleteResponder.createDeleteResponse(deleteResponses)
+    }
+
+    @PostMapping
+    fun applicationDeploymentExists(@RequestBody applicationDeploymentPayload: ApplicationDeploymentPayload): Response {
+
+        val getApplicationDeploymentResponse =
+            applicationDeploymentService.checkApplicationDeploymentsExists(applicationDeploymentPayload.applicationRefs)
+
+        val existsResponse = getApplicationDeploymentResponse.map {
+            ExistsResponse(
+                applicationRef = it.applicationRef,
+                exists = it.exists,
+                success = it.success,
+                message = it.message
+            )
+        }
+
+        return applicationDeploymentDeleteResponder.createExistsResponse(existsResponse)
+    }
+}
+
+data class DeleteResponse(
+    val applicationRef: ApplicationRef,
+    val success: Boolean,
+    val reason: String
+)
+
+data class ExistsResponse(
+    val applicationRef: ApplicationRef,
+    val exists: Boolean,
+    @JsonIgnore val success: Boolean,
+    @JsonIgnore val message: String
+)
+
+@Component
+class ApplicationDeploymentResponder {
+
+    fun createDeleteResponse(deleteResponse: List<DeleteResponse>) =
+        Response(
+            items = deleteResponse,
+            success = deleteResponse.all { it.success },
+            message = deleteResponse.find { it.reason.toUpperCase() != "OK" }?.reason ?: "OK"
+        )
+
+    fun createExistsResponse(existsResponse: List<ExistsResponse>) =
+        Response(
+            items = existsResponse,
+            success = existsResponse.all { it.success },
+            message = existsResponse.find { it.message.toUpperCase() != "OK" }?.message ?: "OK"
+        )
+}
---
+++ src/main/kotlin/no/skatteetaten/aurora/boober/service/ApplicationDeploymentService.kt
@@ -0,0 +1,108 @@
+package no.skatteetaten.aurora.boober.service
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import no.skatteetaten.aurora.boober.controller.v1.ApplicationRef
+import no.skatteetaten.aurora.boober.service.openshift.OpenShiftClient
+import no.skatteetaten.aurora.boober.service.openshift.OpenShiftResourceClient
+import no.skatteetaten.aurora.boober.service.openshift.OpenshiftCommand
+import no.skatteetaten.aurora.boober.service.openshift.OperationType
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+data class ApplicationDeploymentCommand(
+    val cmd: OpenshiftCommand,
+    val applicationRef: ApplicationRef
+)
+
+data class DeleteApplicationDeploymentResponse(
+    val applicationRef: ApplicationRef,
+    val success: Boolean,
+    val message: String
+)
+
+data class GetApplicationDeploymentResponse(
+    val applicationRef: ApplicationRef,
+    val exists: Boolean,
+    val success: Boolean,
+    val message: String
+)
+
+@Service
+class ApplicationDeploymentService(
+    val openShiftClient: OpenShiftClient
+) {
+    val logger: Logger = LoggerFactory.getLogger(ApplicationDeploymentService::class.java)
+
+    fun executeDelete(applicationRefs: List<ApplicationRef>): List<DeleteApplicationDeploymentResponse> {
+        val deleteCommands = createOpenshiftApplicationDeploymentCommands(applicationRefs, OperationType.DELETE)
+
+        return deleteCommands.map {
+            val openshiftResponse =
+                openShiftClient.performOpenShiftCommand(it.applicationRef.namespace, it.cmd)
+
+            if (!openshiftResponse.success) {
+                logger.error(openshiftResponse.exception)
+                DeleteApplicationDeploymentResponse(
+                    applicationRef = it.applicationRef,
+                    success = false,
+                    message = openshiftResponse.exception ?: "An error occured when trying to delete the ApplicationDeployment"
+                )
+            } else {
+                DeleteApplicationDeploymentResponse(
+                    applicationRef = it.applicationRef,
+                    success = openshiftResponse.success,
+                    message = openshiftResponse.exception ?: "Application was successfully deleted"
+                )
+            }
+        }
+    }
+
+    fun checkApplicationDeploymentsExists(applicationrefs: List<ApplicationRef>): List<GetApplicationDeploymentResponse> {
+        val applicationdeploymentGetCommand =
+            createOpenshiftApplicationDeploymentCommands(applicationrefs, OperationType.GET)
+
+        return applicationdeploymentGetCommand.map {
+            val openshiftResponse =
+                openShiftClient.performOpenShiftCommand(it.applicationRef.namespace, it.cmd)
+
+            if (!openshiftResponse.success) {
+                logger.error(openshiftResponse.exception)
+                GetApplicationDeploymentResponse(
+                    applicationRef = it.applicationRef,
+                    exists = false,
+                    success = false,
+                    message = openshiftResponse.exception ?: "An error occured when checking if ApplicationDeployment exists"
+                )
+            } else {
+
+                val applicationDeploymentExists = openshiftResponse.responseBody != null
+
+                GetApplicationDeploymentResponse(
+                    applicationRef = it.applicationRef,
+                    exists = applicationDeploymentExists,
+                    success = true,
+                    message = "OK"
+                )
+            }
+        }
+    }
+
+    private fun createOpenshiftApplicationDeploymentCommands(
+        applicationRefs: List<ApplicationRef>,
+        operationType: OperationType
+    ) =
+        applicationRefs.map {
+            val url = OpenShiftResourceClient.generateUrl(
+                kind = "ApplicationDeployment",
+                name = it.name,
+                namespace = it.namespace
+            )
+            val jsonNode = jacksonObjectMapper().readTree("""{"kind":"applicationdeployment"}""")
+
+            ApplicationDeploymentCommand(
+                cmd = OpenshiftCommand(operationType, url = url, payload = jsonNode),
+                applicationRef = it
+            )
+        }
+}
