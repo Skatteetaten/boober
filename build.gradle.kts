9b6c425d5b83e210f1874ceed922a30c347e5e2f
Erlend Haugsnes <Erlend.Haugsnes@skatteetaten.no>
2019-08-07 10:57:30 +0200

    bumped versions, added sonarqube, removed false from jenkinsfile

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,18 +7,19 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.3.30"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.30"
-    id("org.jlleitschuh.gradle.ktlint") version "7.3.0"
+    id("org.jetbrains.kotlin.jvm") version "1.3.31"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.31"
+    id("org.jlleitschuh.gradle.ktlint") version "8.0.0"
+    id("org.sonarqube") version "2.7.1"
 
-    id("org.springframework.boot") version "2.1.4.RELEASE"
-    id("org.asciidoctor.convert") version "2.0.0"
+    id("org.springframework.boot") version "2.1.6.RELEASE"
+    id("org.asciidoctor.convert") version "2.2.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.0.0"
     id("com.github.ben-manes.versions") version "0.21.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.9"
 
-    id("no.skatteetaten.gradle.aurora") version "2.3.0"
+    id("no.skatteetaten.gradle.aurora") version "2.4.0"
 }
 
 apply(plugin = "spring-cloud-contract")
