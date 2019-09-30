2616050f4a5ce1f3518093e643b59bf5bf5a73e4
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2019-09-30 13:59:39 +0200

    bugfix/AOS-3704 handle empty yaml file as empty json file since jackson does not parse it correctly

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("org.springframework.cloud.contract")
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.41"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
     id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
     id("org.sonarqube") version "2.7.1"
 
