b7cdcfa6183efdf6321cf0cdcf7eb2eb4389b56e
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2019-10-16 10:36:50 +0200

    bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,36 +4,36 @@ plugins {
     id("org.springframework.cloud.contract")
     id("org.jetbrains.kotlin.jvm") version "1.3.50"
     id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
-    id("org.jlleitschuh.gradle.ktlint") version "8.2.0"
-    id("org.sonarqube") version "2.7.1"
+    id("org.jlleitschuh.gradle.ktlint") version "9.0.0"
+    id("org.sonarqube") version "2.8"
 
-    id("org.springframework.boot") version "2.1.8.RELEASE"
+    id("org.springframework.boot") version "2.1.9.RELEASE"
     id("org.asciidoctor.convert") version "2.3.0"
 
-    id("com.gorylenko.gradle-git-properties") version "2.0.0"
-    id("com.github.ben-manes.versions") version "0.22.0"
+    id("com.gorylenko.gradle-git-properties") version "2.2.0"
+    id("com.github.ben-manes.versions") version "0.27.0"
     id("se.patrikerdes.use-latest-versions") version "0.2.12"
-    id("com.adarshr.test-logger") version "1.7.0"
+    id("com.adarshr.test-logger") version "2.0.0"
 
-    id("no.skatteetaten.gradle.aurora") version "2.4.2"
+    id("no.skatteetaten.gradle.aurora") version "3.1.0"
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.2.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.2.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
 
-    implementation("org.eclipse.jgit:org.eclipse.jgit:4.11.0.201803080745-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:5.5.1.201910021850-r")
     implementation("com.github.fge:json-patch:1.9")
     implementation("org.encryptor4j:encryptor4j:0.1.2")
     // The above library uses an vulnerable bcprov, set the fixed version here, hopefully this will work.
     // pr is sent to maintainer
-    implementation("org.bouncycastle:bcprov-jdk15on:1.63")
-    implementation("com.github.ben-manes.caffeine:caffeine:2.7.0")
-    implementation("org.apache.commons:commons-text:1.7")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.64")
+    implementation("com.github.ben-manes.caffeine:caffeine:2.8.0")
+    implementation("org.apache.commons:commons-text:1.8")
 
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
 
-    implementation("com.fkorotkov:kubernetes-dsl:2.2")
+    implementation("com.fkorotkov:kubernetes-dsl:3.0")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-aop")
