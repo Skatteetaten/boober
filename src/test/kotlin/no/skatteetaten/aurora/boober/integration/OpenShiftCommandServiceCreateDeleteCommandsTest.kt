13374272439370e76a7d52724574c86c78ab7834
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2019-10-25 14:28:12 +0200

    fix test

--- src/test/kotlin/no/skatteetaten/aurora/boober/integration/OpenShiftCommandServiceCreateDeleteCommandsTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/boober/integration/OpenShiftCommandServiceCreateDeleteCommandsTest.kt
@@ -1,19 +1,43 @@
 package no.skatteetaten.aurora.boober.integration
 
-// TODO: Hva er egentlig vits å teste her?
+import assertk.assertThat
+import assertk.assertions.isTrue
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.convertValue
+import com.fkorotkov.kubernetes.newConfigMapList
+import io.mockk.clearAllMocks
+import io.mockk.every
+import io.mockk.mockk
+import no.skatteetaten.aurora.boober.model.ApplicationDeploymentRef
+import no.skatteetaten.aurora.boober.service.OpenShiftCommandService
+import no.skatteetaten.aurora.boober.service.openshift.OpenShiftClient
+import no.skatteetaten.aurora.boober.service.openshift.OpenShiftResourceClient
+import no.skatteetaten.aurora.boober.service.openshift.OpenshiftCommand
+import no.skatteetaten.aurora.boober.utils.Instants
+import no.skatteetaten.aurora.boober.utils.addIfNotNull
+import no.skatteetaten.aurora.boober.utils.getResultFiles
+import no.skatteetaten.aurora.boober.utils.jsonMapper
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
+import java.time.Instant
 
-/*
 class OpenShiftCommandServiceCreateDeleteCommandsTest {
 
     val userClient: OpenShiftResourceClient = mockk()
-
-    val openShiftClient = OpenShiftClient(userClient, mockk(), jsonMapper())
-    val openShiftCommandBuilder = OpenShiftCommandService(openShiftClient, mockk())
+    val saClient: OpenShiftResourceClient = mockk()
+    val openShiftClient = OpenShiftClient(userClient, saClient, jsonMapper())
+    val openShiftCommandBuilder = OpenShiftCommandService(openShiftClient)
 
     @BeforeEach
     fun setupTest() {
 
         clearAllMocks()
+        every {
+            saClient.getAuthorizationHeaders()
+        } returns HttpHeaders()
+
         every {
             userClient.getAuthorizationHeaders()
         } returns HttpHeaders()
@@ -27,8 +51,10 @@ class OpenShiftCommandServiceCreateDeleteCommandsTest {
         val namespace = "secretmount"
         val deployId = "abc123"
         val adr = ApplicationDeploymentRef(namespace, name)
-
+        val configMapList = newConfigMapList { }
         val responses = createResponsesFromResultFiles(adr)
+            .addIfNotNull("configmap" to jsonMapper().convertValue<JsonNode>(configMapList))
+
 
         responses.forEach {
             val kind = it.key
@@ -38,11 +64,15 @@ class OpenShiftCommandServiceCreateDeleteCommandsTest {
             every {
                 userClient.get(url, any(), true)
             } returns ResponseEntity.ok(it.value)
+
+            every {
+                saClient.get(url, any(), true)
+            } returns ResponseEntity.ok(it.value)
         }
 
         val commands = openShiftCommandBuilder.createOpenShiftDeleteCommands(name, namespace, deployId)
 
-        listOf("BuildConfig", "DeploymentConfig", "ConfigMap", "ImageStream", "Service", "Secret").forEach {
+        listOf("BuildConfig", "DeploymentConfig", "ImageStream", "Service", "Secret").forEach {
             assertThat(containsKind(it, commands)).isTrue()
         }
     }
@@ -72,4 +102,3 @@ class OpenShiftCommandServiceCreateDeleteCommandsTest {
         }
     }
 }
-*/
