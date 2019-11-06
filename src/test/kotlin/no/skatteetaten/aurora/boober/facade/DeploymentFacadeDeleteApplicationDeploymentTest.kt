9aafed975c3dbbad0eb840a9a3b0475a60b14c14
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2019-11-06 18:49:41 +0100

    try with testInstance

--- src/test/kotlin/no/skatteetaten/aurora/boober/facade/DeploymentFacadeDeleteApplicationDeploymentTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/boober/facade/DeploymentFacadeDeleteApplicationDeploymentTest.kt
@@ -8,6 +8,7 @@ import assertk.assertions.isTrue
 import no.skatteetaten.aurora.boober.model.ApplicationRef
 import okhttp3.mockwebserver.MockResponse
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -15,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest
     webEnvironment = SpringBootTest.WebEnvironment.NONE,
     properties = ["integrations.openshift.retries=0"]
 )
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class DeploymentFacadeDeleteApplicationDeploymentTest : AbstractSpringBootTest() {
 
     @Autowired
