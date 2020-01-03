561a67b46322fb6c3dd523b86d388f4baf3af05a
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2020-01-03 14:15:25 +0100

    bump dependencies and refactor sts/certificate features

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ import com.adarshr.gradle.testlogger.theme.ThemeType
 
 plugins {
     id("org.springframework.cloud.contract")
-    id("org.jetbrains.kotlin.jvm") version "1.3.50"
-    id("org.jetbrains.kotlin.plugin.spring") version "1.3.50"
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.3.61"
     id("org.jlleitschuh.gradle.ktlint") version "9.1.1"
     id("org.sonarqube") version "2.8"
 
-    id("org.springframework.boot") version "2.2.1.RELEASE"
-    id("org.asciidoctor.convert") version "2.3.0"
+    id("org.springframework.boot") version "2.2.2.RELEASE"
+    id("org.asciidoctor.convert") version "2.4.0"
 
     id("com.gorylenko.gradle-git-properties") version "2.2.0"
     id("com.github.ben-manes.versions") version "0.27.0"
@@ -19,8 +19,8 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.3.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
 
     implementation("org.eclipse.jgit:org.eclipse.jgit:4.11.0.201803080745-r")
     implementation("com.github.fge:json-patch:1.9")
@@ -46,8 +46,8 @@ dependencies {
 
     testImplementation("io.mockk:mockk:1.9.3")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.20")
-    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.0.2")
-    testImplementation("com.ninja-squad:springmockk:1.1.3")
+    testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.0.4")
+    testImplementation("com.ninja-squad:springmockk:2.0.0")
 }
 
 testlogger {
