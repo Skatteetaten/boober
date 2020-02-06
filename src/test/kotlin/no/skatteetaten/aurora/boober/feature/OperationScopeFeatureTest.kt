ab9a1d5e039ddbd0a3dcb54799c12268823ce0a5
Raymond Koteng <Raymond.Koteng@skatteetaten.no>
2020-02-06 13:29:25 +0100

    Merge tests about OperationScopeFeature

--- src/test/kotlin/no/skatteetaten/aurora/boober/feature/OperationScopeFeatureTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/boober/feature/OperationScopeFeatureTest.kt
@@ -1,35 +1,65 @@
 package no.skatteetaten.aurora.boober.feature
 
 import assertk.assertThat
+import assertk.assertions.isEmpty
 import assertk.assertions.isEqualTo
-import io.mockk.every
-import io.mockk.mockk
-import no.skatteetaten.aurora.boober.controller.security.User
-import no.skatteetaten.aurora.boober.service.UserDetailsProvider
+import assertk.assertions.isNull
 import no.skatteetaten.aurora.boober.utils.AbstractFeatureTest
 import org.junit.jupiter.api.Test
 
-class OperationScopeFeatureTest : AbstractFeatureTest() {
+class OperationScopeFeatureTest {
 
-    override val feature: Feature
-        get() = OperationScopeFeature("my-config-value")
+    class OperationScopeFeatureWithValue : AbstractFeatureTest() {
 
-    val userDetailsProvider: UserDetailsProvider = mockk()
+        override val feature: Feature
+            get() = OperationScopeFeature("my-config-value")
+    }
+
+    class OperationScopeFeatureWithoutValue : AbstractFeatureTest() {
+
+        override val feature: Feature
+            get() = OperationScopeFeature("")
+    }
 
     @Test
-    fun `check operation scope label`() {
+    fun `set operationScope label`() {
 
-        every { userDetailsProvider.getAuthenticatedUser() } returns User("hero", "token")
+        val feature = OperationScopeFeatureWithValue()
 
-        val resources = modifyResources("{}", createEmptyImageStream(), createEmptyDeploymentConfig())
+        val imageStream = feature.createEmptyImageStream()
+        val emptyDeploymentConfig = feature.createEmptyDeploymentConfig()
+
+        val resources = feature.modifyResources(
+            "{}",
+            imageStream,
+            emptyDeploymentConfig
+        )
         val label = mapOf("operationScope" to "my-config-value")
 
         resources.forEach {
-            assertThat(it).auroraResourceModifiedByThisFeatureWithComment("Added operationScope")
+            assertThat(it.sources.first().comment).isEqualTo("Added operationScope")
             assertThat(it.resource.metadata.labels).isEqualTo(label)
         }
 
         val dcResource = resources.last()
         assertThat(dcResource.sources.last().comment).isEqualTo("Added operationScope")
+    }
+
+    @Test
+    fun `operationScope is not set`() {
+        val feature = OperationScopeFeatureWithoutValue()
+        val emptyImageStream = feature.createEmptyImageStream()
+        val emptyDeploymentConfig = feature.createEmptyDeploymentConfig()
+
+        val resources = feature.modifyResources(
+            "{}",
+            emptyImageStream,
+            emptyDeploymentConfig
+        )
+
+        resources.forEach {
+            assertThat(it.sources).isEmpty()
+            assertThat(it.resource.metadata.labels).isNull()
+        }
     }
 }
