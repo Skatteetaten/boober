63b9960cfa7bc9d4db609630cd3661707e33abba
Raymond Koteng <raymondkoteng@gmail.com>
2020-02-07 13:00:14 +0100

    feature/AOS-4016 operation scope (#357)
    * Add label for operationScope if value is provided from config file.
    
    * Merge tests about OperationScopeFeature
    
    * Code formatting

---
+++ src/main/kotlin/no/skatteetaten/aurora/boober/feature/OperationScopeFeature.kt
@@ -0,0 +1,32 @@
+package no.skatteetaten.aurora.boober.feature
+
+import no.skatteetaten.aurora.boober.model.AuroraConfigFieldHandler
+import no.skatteetaten.aurora.boober.model.AuroraContextCommand
+import no.skatteetaten.aurora.boober.model.AuroraDeploymentSpec
+import no.skatteetaten.aurora.boober.model.AuroraResource
+import no.skatteetaten.aurora.boober.utils.addIfNotNull
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+
+@Service
+@ConditionalOnProperty("integrations.operations.scope")
+class OperationScopeFeature(
+    @Value("\${integrations.operations.scope}") val operationScopeConfiguration: String
+) : Feature {
+    override fun handlers(header: AuroraDeploymentSpec, cmd: AuroraContextCommand): Set<AuroraConfigFieldHandler> =
+        emptySet()
+
+    override fun modify(adc: AuroraDeploymentSpec, resources: Set<AuroraResource>, cmd: AuroraContextCommand) {
+
+        if (operationScopeConfiguration.isBlank()) {
+            return
+        }
+
+        resources.forEach {
+            modifyResource(it, "Added operationScope")
+            it.resource.metadata.labels =
+                mapOf("operationScope" to operationScopeConfiguration).addIfNotNull(it.resource.metadata?.labels)
+        }
+    }
+}
---
+++ src/test/kotlin/no/skatteetaten/aurora/boober/feature/OperationScopeFeatureTest.kt
@@ -0,0 +1,65 @@
+package no.skatteetaten.aurora.boober.feature
+
+import assertk.assertThat
+import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNull
+import no.skatteetaten.aurora.boober.utils.AbstractFeatureTest
+import org.junit.jupiter.api.Test
+
+class OperationScopeFeatureTest {
+
+    class OperationScopeFeatureWithValue : AbstractFeatureTest() {
+
+        override val feature: Feature
+            get() = OperationScopeFeature("my-config-value")
+    }
+
+    class OperationScopeFeatureWithoutValue : AbstractFeatureTest() {
+
+        override val feature: Feature
+            get() = OperationScopeFeature("")
+    }
+
+    @Test
+    fun `set operationScope label`() {
+
+        val feature = OperationScopeFeatureWithValue()
+
+        val imageStream = feature.createEmptyImageStream()
+        val emptyDeploymentConfig = feature.createEmptyDeploymentConfig()
+
+        val resources = feature.modifyResources(
+            "{}",
+            imageStream,
+            emptyDeploymentConfig
+        )
+        val label = mapOf("operationScope" to "my-config-value")
+
+        resources.forEach {
+            assertThat(it.sources.first().comment).isEqualTo("Added operationScope")
+            assertThat(it.resource.metadata.labels).isEqualTo(label)
+        }
+
+        val dcResource = resources.last()
+        assertThat(dcResource.sources.last().comment).isEqualTo("Added operationScope")
+    }
+
+    @Test
+    fun `operationScope is not set`() {
+        val feature = OperationScopeFeatureWithoutValue()
+        val emptyImageStream = feature.createEmptyImageStream()
+        val emptyDeploymentConfig = feature.createEmptyDeploymentConfig()
+
+        val resources = feature.modifyResources(
+            "{}",
+            emptyImageStream,
+            emptyDeploymentConfig
+        )
+
+        resources.forEach {
+            assertThat(it.sources).isEmpty()
+            assertThat(it.resource.metadata.labels).isNull()
+        }
+    }
+}
