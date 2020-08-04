43e0237bbad45585a0d01aba96cc050911cba45f
Bjarte S. Karlsen <bjarte.karlsen@skatteetaten.no>
2020-08-04 09:39:24 +0200

    bugfix/AOS-4417 added disabled webseal feature and toggle betwwen them (#381)

--- src/main/kotlin/no/skatteetaten/aurora/boober/feature/WebsealFeature.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/feature/WebsealFeature.kt
@@ -1,37 +1,39 @@
 package no.skatteetaten.aurora.boober.feature
 
+import no.skatteetaten.aurora.boober.model.AuroraConfigException
 import no.skatteetaten.aurora.boober.model.AuroraConfigFieldHandler
 import no.skatteetaten.aurora.boober.model.AuroraContextCommand
 import no.skatteetaten.aurora.boober.model.AuroraDeploymentSpec
 import no.skatteetaten.aurora.boober.model.AuroraResource
+import no.skatteetaten.aurora.boober.utils.ConditionalOnPropertyMissingOrEmpty
 import no.skatteetaten.aurora.boober.utils.boolean
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
 
 val WEBSEAL_ROLES_ANNOTATION: String = "marjory.sits.no/route.roles"
 val WEBSEAL_DONE_ANNOTATION: String = "marjory.sits.no-routes-config.done"
 
+@ConditionalOnPropertyMissingOrEmpty("integrations.skap.url")
+@Service
+class WebsealDisabledFeature : AbstractWebsealFeature("") {
+
+    override fun validate(
+        adc: AuroraDeploymentSpec,
+        fullValidation: Boolean,
+        cmd: AuroraContextCommand
+    ): List<Exception> {
+        return adc.featureEnabled("webseal") { field ->
+            listOf(AuroraConfigException("Webseal is not supported."))
+        } ?: emptyList()
+    }
+}
+
+@ConditionalOnProperty(value = ["integrations.skap.url"])
 @Service
 class WebsealFeature(
-    @Value("\${boober.webseal.suffix}") val webSealSuffix: String
-) : Feature {
-
-    override fun enable(header: AuroraDeploymentSpec): Boolean {
-        return !header.isJob
-    }
-    override fun handlers(header: AuroraDeploymentSpec, cmd: AuroraContextCommand): Set<AuroraConfigFieldHandler> {
-        return setOf(
-            AuroraConfigFieldHandler(
-                "webseal",
-                defaultValue = false,
-                validator = { it.boolean() },
-                canBeSimplifiedConfig = true
-            ),
-            AuroraConfigFieldHandler("webseal/host"),
-            AuroraConfigFieldHandler("webseal/strict", validator = { it.boolean() }),
-            AuroraConfigFieldHandler("webseal/roles")
-        )
-    }
+    @Value("\${boober.webseal.suffix}") suffix: String
+) : AbstractWebsealFeature(suffix) {
 
     override fun generate(adc: AuroraDeploymentSpec, cmd: AuroraContextCommand): Set<AuroraResource> {
         return adc.featureEnabled("webseal") { field ->
@@ -52,6 +54,29 @@ class WebsealFeature(
 
             setOf(auroraRoute.generateOpenShiftRoute(adc.namespace, adc.name, webSealSuffix).generateAuroraResource())
         } ?: emptySet()
+    }
+}
+
+abstract class AbstractWebsealFeature(
+    val webSealSuffix: String
+) : Feature {
+
+    override fun enable(header: AuroraDeploymentSpec): Boolean {
+        return !header.isJob
+    }
+
+    override fun handlers(header: AuroraDeploymentSpec, cmd: AuroraContextCommand): Set<AuroraConfigFieldHandler> {
+        return setOf(
+            AuroraConfigFieldHandler(
+                "webseal",
+                defaultValue = false,
+                validator = { it.boolean() },
+                canBeSimplifiedConfig = true
+            ),
+            AuroraConfigFieldHandler("webseal/host"),
+            AuroraConfigFieldHandler("webseal/strict", validator = { it.boolean() }),
+            AuroraConfigFieldHandler("webseal/roles")
+        )
     }
 
     fun willCreateResource(adc: AuroraDeploymentSpec): Boolean {
---
+++ src/test/kotlin/no/skatteetaten/aurora/boober/feature/WebsealFeatureDisabledTest.kt
@@ -0,0 +1,21 @@
+package no.skatteetaten.aurora.boober.feature
+
+import assertk.assertThat
+import no.skatteetaten.aurora.boober.utils.AbstractFeatureTest
+import no.skatteetaten.aurora.boober.utils.singleApplicationError
+import org.junit.jupiter.api.Test
+
+class WebsealFeatureDisabledTest : AbstractFeatureTest() {
+    override val feature: Feature
+        get() = WebsealDisabledFeature()
+
+    @Test
+    fun `get error if trying to create webseal opening when feature disabled`() {
+
+        assertThat { generateResources(
+            """{ 
+                "webseal" : true
+           }"""
+        ) }.singleApplicationError("Webseal is not supported")
+    }
+}
