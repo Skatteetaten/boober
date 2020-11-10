25f59d0da92cd1701be5e47f3b04dda184fbb6be
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2020-11-10 12:17:26 +0100

    added bigip url test

--- src/test/kotlin/no/skatteetaten/aurora/boober/unit/OpenshiftApiUrlsTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/boober/unit/OpenshiftApiUrlsTest.kt
@@ -22,7 +22,8 @@ class OpenshiftApiUrlsTest {
         USER("user", "foo", null, "/apis/user.openshift.io/v1/users/foo"),
         TEMPLATE("processedtemplate", null, "bar", "/oapi/v1/namespaces/bar/processedtemplates"),
         PROJECT("project", "foo", null, "/apis/project.openshift.io/v1/projects/foo"),
-        SERVICE("service", "foo", "bar", "/api/v1/namespaces/bar/services/foo")
+        SERVICE("service", "foo", "bar", "/api/v1/namespaces/bar/services/foo"),
+        BIGIP("bigip", "foo", "bar", "/apis/skatteetaten.no/v1/namespaces/bar/bigips/foo")
     }
 
     @ParameterizedTest
