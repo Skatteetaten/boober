2fe902166ab1baf16966483a7fbc1bf6f08292ef
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2020-12-14 15:42:25 +0100

    added some more tests

--- src/main/kotlin/no/skatteetaten/aurora/boober/feature/HerkimerVaultFeature.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/feature/HerkimerVaultFeature.kt
@@ -9,34 +9,39 @@ import no.skatteetaten.aurora.boober.model.AuroraContextCommand
 import no.skatteetaten.aurora.boober.model.AuroraDeploymentSpec
 import no.skatteetaten.aurora.boober.model.AuroraResource
 import no.skatteetaten.aurora.boober.model.addEnvVarsToMainContainers
+import no.skatteetaten.aurora.boober.model.findSubKeys
 import no.skatteetaten.aurora.boober.service.HerkimerService
+import no.skatteetaten.aurora.boober.service.ResourceHerkimer
 import no.skatteetaten.aurora.boober.service.ResourceKind
+import no.skatteetaten.aurora.boober.utils.addIfNotNull
 import no.skatteetaten.aurora.boober.utils.boolean
 import no.skatteetaten.aurora.boober.utils.createEnvVarRefs
+import no.skatteetaten.aurora.boober.utils.ensureEndsWith
+import no.skatteetaten.aurora.boober.utils.ensureStartWith
 import no.skatteetaten.aurora.boober.utils.findResourcesByType
 import no.skatteetaten.aurora.boober.utils.jsonMapper
-import no.skatteetaten.aurora.boober.utils.notBlank
 import no.skatteetaten.aurora.boober.utils.oneOf
 import org.apache.commons.codec.binary.Base64
 import org.springframework.stereotype.Service
 
 private const val FEATURE_FIELD = "resources"
+private const val ANNOTATION_HERKIMER_PREFIX = "herkimer.skatteetaten.no/prefix"
 
 @Service
 class HerkimerVaultFeature(
     val herkimerService: HerkimerService
 ) : Feature {
     override fun handlers(header: AuroraDeploymentSpec, cmd: AuroraContextCommand): Set<AuroraConfigFieldHandler> {
-        return header.getSubKeyValues(FEATURE_FIELD).flatMap { key ->
+        return cmd.applicationFiles.findSubKeys(FEATURE_FIELD).flatMap { key ->
             setOf(
                 AuroraConfigFieldHandler(
                     "$FEATURE_FIELD/$key/enabled",
                     defaultValue = true,
                     validator = { it.boolean() }),
-                AuroraConfigFieldHandler("$FEATURE_FIELD/$key/prefix", defaultValue = ""),
+                AuroraConfigFieldHandler("$FEATURE_FIELD/$key/prefix"),
                 AuroraConfigFieldHandler(
                     "$FEATURE_FIELD/$key/serviceClass",
-                    validator = { node -> node.oneOf(ResourceKind.values().map { it.toString()})}),
+                    validator = { node -> node.oneOf(ResourceKind.values().map { it.toString() }) }),
                 AuroraConfigFieldHandler(
                     "$FEATURE_FIELD/$key/multiple",
                     defaultValue = false,
@@ -45,61 +50,99 @@ class HerkimerVaultFeature(
         }.toSet()
     }
 
+    override fun createContext(
+        spec: AuroraDeploymentSpec,
+        cmd: AuroraContextCommand,
+        validationContext: Boolean
+    ): Map<String, Any> {
+        val herkimerVaultResources =
+            spec.findAllConfiguredHerkimerResources()
+        val resources = mapOf("resources" to herkimerVaultResources)
+        if (validationContext) {
+            return resources
+        }
+
+        //TODO: for now this will only work with Database
+        val herkimerResources: Map<HerkimerVaultResource, List<ResourceHerkimer>> =
+            herkimerVaultResources.associateWith {
+                herkimerService.getClaimedResources(spec.applicationDeploymentId, it.serviceClass)
+            }
+
+        val secrets: Map<HerkimerVaultResource, List<Secret>> = herkimerResources.map { (config, resources) ->
+            val secrets = if (!config.multiple) {
+                listOf(generateHerkimerSecret(resources.first(), spec))
+            } else {
+                resources.map {
+                    generateHerkimerSecret(it, spec)
+                }
+            }
+            config to secrets
+        }.toMap()
+
+        return resources.addIfNotNull("secrets" to secrets)
+    }
+
     override fun validate(
         adc: AuroraDeploymentSpec,
         fullValidation: Boolean,
         context: Map<String, Any>
     ): List<Exception> {
-        val configuredHerkimerVaultResources = adc.findAllConfiguredHerkimerResources()
+
+        //TODO: Need to validate that each kind is there only once
+        val configuredHerkimerVaultResources = context["resources"] as List<HerkimerVaultResource>
 
         return validateNotExistsResourcesWithMultipleTrueAndFalseWithSamePrefix(configuredHerkimerVaultResources)
+        //only deep validate
+        //TODO validate that if there are more responses for a single entry it should fail
     }
 
-    override fun generate(adc: AuroraDeploymentSpec,  context: Map<String, Any>): Set<AuroraResource> {
+    override fun generate(adc: AuroraDeploymentSpec, context: Map<String, Any>): Set<AuroraResource> {
         if (!adc.isFeatureEnabled()) return emptySet()
 
-        val herkimerVaultResources = adc.findAllConfiguredHerkimerResources().groupBy { it.prefix }
-        val secrets: List<Secret> = herkimerVaultResources.flatMap { (prefix, resources) ->
-            //TODO: refactor
-            resources.flatMap {
-                herkimerService.getClaimedResources(adc.applicationDeploymentId, it.serviceClass).flatMap { resource ->
-                    resource.claims.map { claims ->
-                        val values = jsonMapper().convertValue<Map<String, String>>(claims.credentials)
-                        newSecret {
-                            metadata {
-                                name = "${adc.name}-${resource.name}-resources"
-                                namespace = adc.namespace
-                                annotations = mapOf(
-                                    "prefix" to prefix
-                                )
-                            }
-                            data = values.mapValues { content -> Base64.encodeBase64String(content.value.toByteArray()) }
-                        }
-                    }
-                }
-            }
-        }
+        val herkimerVaultResources = context["secrets"] as Map<HerkimerVaultResource, List<Secret>>
 
-        return secrets.map {
-            it.generateAuroraResource()
+        return herkimerVaultResources.values.flatMap { secrets ->
+            secrets.map {
+                it.generateAuroraResource()
+            }
         }.toSet()
     }
 
-    override fun modify(adc: AuroraDeploymentSpec, resources: Set<AuroraResource>,  context: Map<String, Any>) {
+    private fun generateHerkimerSecret(response: ResourceHerkimer, adc: AuroraDeploymentSpec): Secret {
+
+        //TODO: Should this be first and should it be called credentials?
+        val values = jsonMapper().convertValue<Map<String, String>>(response.claims.first().credentials)
+        return newSecret {
+            metadata {
+                name = response.name.ensureStartWith(adc.name, "-")
+                    .ensureEndsWith("resources", "-")
+                    .toLowerCase().replace("_", "-")
+                namespace = adc.namespace
+            }
+            data = values.mapValues { content -> Base64.encodeBase64String(content.value.toByteArray()) }
+        }
+    }
+
+    override fun modify(adc: AuroraDeploymentSpec, resources: Set<AuroraResource>, context: Map<String, Any>) {
         if (!adc.isFeatureEnabled()) return
 
-        val herkimerVaultResources = adc.findAllConfiguredHerkimerResources()
-        val prefix: String = adc["$FEATURE_FIELD/prefix"]
-        val envVars = resources.findResourcesByType<Secret>(suffix = "herkimervault")
-            .mapIndexed { index, secret ->
-                secret.createEnvVarRefs(prefix = "${prefix}_${index}_", forceUpperCaseForEnvVarName = false)
-            }.flatten()
+        val herkimerVaultResources = context["secrets"] as Map<HerkimerVaultResource, List<Secret>>
+
+        val envVars =herkimerVaultResources.flatMap { (config, secrets) ->
+            if(config.multiple) {
+                secrets.mapIndexed { index, secret ->
+                    secret.createEnvVarRefs(prefix = "${config.prefix}_${index}_", forceUpperCaseForEnvVarName = false)
+                }
+            }else {
+                listOf(secrets.first().createEnvVarRefs(prefix="${config.prefix}_"))
+            }
+        }.flatten()
 
         resources.addEnvVarsToMainContainers(envVars, javaClass)
     }
 
     private fun AuroraDeploymentSpec.findAllConfiguredHerkimerResources() =
-        this.getSubKeyValues("$FEATURE_FIELD").mapNotNull {
+        this.getSubKeyValues(FEATURE_FIELD).mapNotNull {
             this.findConfiguredHerkimerResource(it)
         }
 
@@ -107,7 +150,7 @@ class HerkimerVaultFeature(
         val enabled: Boolean = this["$FEATURE_FIELD/$resourceKey/enabled"]
         if (!enabled) return null
         return HerkimerVaultResource(
-            prefix = this["$FEATURE_FIELD/$resourceKey/prefix"],
+            prefix = this.getOrNull<String>("$FEATURE_FIELD/$resourceKey/prefix") ?: resourceKey,
             serviceClass = this["$FEATURE_FIELD/$resourceKey/serviceClass"],
             multiple = this["$FEATURE_FIELD/$resourceKey/multiple"]
 
@@ -136,4 +179,9 @@ data class HerkimerVaultResource(
     val prefix: String,
     val serviceClass: ResourceKind,
     val multiple: Boolean
+)
+
+data class HerkimerVaultResourceResponse(
+    val request: HerkimerVaultResource,
+    val response: ResourceHerkimer
 )
---
+++ src/test/kotlin/no/skatteetaten/aurora/boober/feature/HerkimerVaultFeatureTest.kt
@@ -0,0 +1,139 @@
+package no.skatteetaten.aurora.boober.feature
+
+import assertk.assertThat
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.convertValue
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import io.mockk.every
+import io.mockk.mockk
+import no.skatteetaten.aurora.boober.service.HerkimerService
+import no.skatteetaten.aurora.boober.service.ResourceClaimHerkimer
+import no.skatteetaten.aurora.boober.service.ResourceHerkimer
+import no.skatteetaten.aurora.boober.service.ResourceKind
+import no.skatteetaten.aurora.boober.utils.AbstractFeatureTest
+import no.skatteetaten.aurora.mockmvc.extensions.mockwebserver.HttpMock
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
+
+class HerkimerVaultFeatureTest : AbstractFeatureTest() {
+    override val feature: Feature
+        get() = HerkimerVaultFeature(
+            herkimerService = herkimerService
+        )
+
+    private val herkimerService: HerkimerService = mockk()
+
+    @AfterEach
+    fun after() {
+        HttpMock.clearAllHttpMocks()
+    }
+
+    @Test
+    fun `verify creating secret from herkimer`() {
+
+        mockHerkimerDatabase(
+            createResourceHerkimer("ski-postgres", mapOf("foo" to "bar", "bar" to "baz"))
+        )
+
+        val (dc, secret) = generateResources(
+            """{ 
+                "resources": {
+                    "ski" : {
+                        "serviceClass": "DatabaseInstance",
+                        "multiple" : true
+                    }
+                }
+           }""",
+            createdResources = 1,
+            resources = mutableSetOf(createEmptyDeploymentConfig())
+        )
+        assertThat(dc).auroraResourceMatchesFile("dc-multiple-single.json")
+        assertThat(secret).auroraResourceCreatedByThisFeature().auroraResourceMatchesFile("secret-ski.json")
+    }
+
+    @Test
+    fun `verify creating secret from herkimer two results`() {
+
+        mockHerkimerDatabase(
+            createResourceHerkimer("ski-postgres", mapOf("foo" to "bar", "bar" to "baz")),
+            createResourceHerkimer("pros-postgres", mapOf("foo" to "baz", "bar" to "foo"))
+        )
+
+        val (dc, secret1, secret2) = generateResources(
+            """{ 
+                "resources": {
+                    "ski" : {
+                        "serviceClass": "DatabaseInstance",
+                        "multiple" : true
+                    }
+                }
+           }""",
+            createdResources = 2,
+            resources = mutableSetOf(createEmptyDeploymentConfig())
+        )
+        assertThat(dc).auroraResourceMatchesFile("dc-multiple-multiple.json")
+        assertThat(secret1).auroraResourceCreatedByThisFeature().auroraResourceMatchesFile("secret-ski.json")
+        assertThat(secret2).auroraResourceCreatedByThisFeature().auroraResourceMatchesFile("secret-pros.json")
+    }
+
+    @Test
+    fun `verify creating secret from herkimer for single response`() {
+
+        mockHerkimerDatabase(
+            createResourceHerkimer("ski-postgres", mapOf("foo" to "bar", "bar" to "baz"))
+        )
+
+        val (dc, secret) = generateResources(
+            """{ 
+                "resources": {
+                    "ski" : {
+                        "serviceClass": "DatabaseInstance"
+                    }
+                }
+           }""",
+            createdResources = 1,
+            resources = mutableSetOf(createEmptyDeploymentConfig())
+        )
+        assertThat(dc).auroraResourceMatchesFile("dc-single.json")
+        assertThat(secret).auroraResourceCreatedByThisFeature().auroraResourceMatchesFile("secret-ski.json")
+    }
+
+    private fun mockHerkimerDatabase(
+        vararg resourceHerkimer: ResourceHerkimer
+    ) {
+        every {
+            herkimerService.getClaimedResources("1234567890", ResourceKind.DatabaseInstance)
+        } returns resourceHerkimer.toList()
+    }
+
+    private fun createResourceHerkimer(
+        name: String,
+        data: Map<String, String>
+    ) = ResourceHerkimer(
+        id = "0",
+        name = name,
+        kind = ResourceKind.DatabaseInstance,
+        ownerId = "1234567890",
+        claims = listOf(createResourceClaim(data = jacksonObjectMapper().convertValue(data))),
+        createdDate = LocalDateTime.now(),
+        modifiedDate = LocalDateTime.now(),
+        createdBy = "aurora",
+        modifiedBy = "aurora",
+        parentId = null
+    )
+
+    private fun createResourceClaim(
+        data: JsonNode = jacksonObjectMapper().createObjectNode()
+    ) =
+        ResourceClaimHerkimer(
+            id = "0L",
+            ownerId = "owner",
+            resourceId = 0L,
+            credentials = data,
+            createdDate = LocalDateTime.now(),
+            modifiedDate = LocalDateTime.now(),
+            createdBy = "aurora",
+            modifiedBy = "aurora"
+        )
+}
