f290bedc5d3c8a001a6398c4a27f6a187936b56b
Bent Andre Solheim <BentAndre.Solheim@skatteetaten.no>
2021-08-10 13:06:34 +0200

    Removed unused import

--- src/test/kotlin/no/skatteetaten/aurora/boober/facade/DeployFacadeTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/boober/facade/DeployFacadeTest.kt
@@ -1,7 +1,6 @@
 package no.skatteetaten.aurora.boober.facade
 
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.params.ParameterizedTest
