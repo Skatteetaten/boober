b6b047d302c1675fcefcebe2a8e4c6b71a2fb5cc
Frode Mathiesen <Frode.Mathiesen@skatteetaten.no>
2021-08-13 08:56:06 +0200

    AOS-5327: implementerer generering av CRD for alerts

---
+++ src/main/kotlin/no/skatteetaten/aurora/boober/feature/AlertsFeature.kt
@@ -0,0 +1,201 @@
+package no.skatteetaten.aurora.boober.feature
+
+import com.fkorotkov.kubernetes.newObjectMeta
+import no.skatteetaten.aurora.boober.model.AuroraConfigFieldHandler
+import no.skatteetaten.aurora.boober.model.AuroraConfigFile
+import no.skatteetaten.aurora.boober.model.AuroraContextCommand
+import no.skatteetaten.aurora.boober.model.AuroraDeploymentSpec
+import no.skatteetaten.aurora.boober.model.AuroraResource
+import no.skatteetaten.aurora.boober.model.findSubKeys
+import no.skatteetaten.aurora.boober.model.openshift.AlertConfig
+import no.skatteetaten.aurora.boober.model.openshift.AlertSpec
+import no.skatteetaten.aurora.boober.model.openshift.Alerts
+import no.skatteetaten.aurora.boober.model.openshift.ApplicationConfig
+import no.skatteetaten.aurora.boober.model.openshift.PrometheusConfig
+import no.skatteetaten.aurora.boober.service.AuroraDeploymentSpecValidationException
+import no.skatteetaten.aurora.boober.utils.boolean
+import no.skatteetaten.aurora.boober.utils.int
+import no.skatteetaten.aurora.boober.utils.oneOf
+import org.springframework.stereotype.Service
+
+data class AlertConfiguration(
+    val enabled: Boolean,
+    val expr: String,
+    val delay: Int,
+    val connection: String,
+    val severity: String
+)
+
+@Service
+class AlertsFeature : Feature {
+
+    enum class Errors(val message: String) {
+        MissingAlertEnabledProp("alerts/<name>/enabled or alertsDefaults/enabled is required in alert configuration"),
+        MissingAlertConnectionProp("alerts/<name>/connection or alertsDefaults/connection is required in alert configuration"),
+        MissingAlertDelayProp("alerts/<name>/delay or alertsDefaults/delay is required in alert configuration"),
+        MissingAlertExpression("alerts/<name>/expr is required in alert configuration"),
+        MissingAlertSeverity("alerts/<name>/severity is required in alert configuration")
+    }
+
+    val defaultsName = "alertsDefaults"
+    val featureName = "alerts"
+
+    val alertsConfigKeys: List<String> = listOf(
+        "enabled",
+        "expr",
+        "delay",
+        "connection",
+        "severity"
+    )
+
+    override fun enable(header: AuroraDeploymentSpec): Boolean {
+        return !header.isJob
+    }
+
+    override fun handlers(header: AuroraDeploymentSpec, cmd: AuroraContextCommand): Set<AuroraConfigFieldHandler> {
+        val definedAlerts = cmd.applicationFiles.getDefinedAlerts()
+            .flatMap { name ->
+                setOf(
+                    AuroraConfigFieldHandler("$defaultsName/enabled"),
+                    AuroraConfigFieldHandler("$defaultsName/connection"),
+                    AuroraConfigFieldHandler("$defaultsName/delay"),
+                    AuroraConfigFieldHandler("$featureName/$name/enabled", { it.boolean() }),
+                    AuroraConfigFieldHandler("$featureName/$name/expr"),
+                    AuroraConfigFieldHandler("$featureName/$name/delay", { it.int() }),
+                    AuroraConfigFieldHandler("$featureName/$name/connection"),
+                    AuroraConfigFieldHandler("$featureName/$name/severity", validator = { node ->
+                        node?.oneOf(listOf("warning", "critical"))
+                    })
+                )
+            }.toSet()
+        return definedAlerts
+    }
+
+    override fun validate(
+        adc: AuroraDeploymentSpec,
+        fullValidation: Boolean,
+        context: FeatureContext
+    ): List<Exception> {
+        val alarms = adc.getDefinedAlerts()
+
+        val isExprConfigMissing = alarms.any {
+            adc.getOrNull<String>("$featureName/$it/expr").isNullOrEmpty()
+        }
+
+        val isEnabledPropertyMissing = alarms.any {
+            adc.getOrNull<String>("$featureName/$it/enabled").isNullOrEmpty() &&
+                    adc.getOrNull<String>("$defaultsName/enabled").isNullOrEmpty()
+        }
+
+        val isConnectionPropertyMissing = alarms.any {
+            adc.getOrNull<String>("$featureName/$it/connection").isNullOrEmpty() &&
+                    adc.getOrNull<String>("$defaultsName/connection").isNullOrEmpty()
+        }
+
+        val isDelayPropertyMissing = alarms.any {
+            adc.getOrNull<String>("$featureName/$it/delay").isNullOrEmpty() &&
+                    adc.getOrNull<String>("$defaultsName/delay").isNullOrEmpty()
+        }
+
+        val isSeverityMissing = alarms.any {
+            adc.getOrNull<String>("$featureName/$it/severity").isNullOrEmpty()
+        }
+
+        val validationErrors = mutableListOf<AuroraDeploymentSpecValidationException>()
+        if (isExprConfigMissing) {
+            validationErrors.add(AuroraDeploymentSpecValidationException(Errors.MissingAlertExpression.message))
+        }
+
+        if (isEnabledPropertyMissing) {
+            validationErrors.add(AuroraDeploymentSpecValidationException(Errors.MissingAlertEnabledProp.message))
+        }
+
+        if (isConnectionPropertyMissing) {
+            validationErrors.add(AuroraDeploymentSpecValidationException(Errors.MissingAlertConnectionProp.message))
+        }
+
+        if (isDelayPropertyMissing) {
+            validationErrors.add(AuroraDeploymentSpecValidationException(Errors.MissingAlertDelayProp.message))
+        }
+
+        if (isSeverityMissing) {
+            validationErrors.add(AuroraDeploymentSpecValidationException(Errors.MissingAlertSeverity.message))
+        }
+
+        return validationErrors
+    }
+
+    override fun generate(adc: AuroraDeploymentSpec, context: FeatureContext): Set<AuroraResource> {
+        return adc.getDefinedAlerts()
+            .flatMap { generateAlertResources(it, adc) }
+            .toSet()
+    }
+
+    private fun List<AuroraConfigFile>.getDefinedAlerts(): Set<String> {
+        return this.findSubKeys(featureName).filter { !alertsConfigKeys.contains(it) }.toSet()
+    }
+
+    private fun AuroraDeploymentSpec.getDefinedAlerts(): Set<String> {
+        return this.findSubKeysRaw(featureName)
+            .toSet()
+    }
+
+    fun generateAlertResources(alertName: String, adc: AuroraDeploymentSpec): Set<AuroraResource> {
+        val alertConfig = adc.extractConfiguration(alertName)
+
+        if (!alertConfig.enabled) {
+            return emptySet()
+        }
+
+        val alerts = Alerts(
+            _metadata = newObjectMeta {
+                name = alertName
+                namespace = adc.namespace
+            },
+            spec = AlertSpec(
+                ApplicationConfig(adc.affiliation, adc.cluster, adc.envName, adc.name),
+                PrometheusConfig(alertConfig.expr),
+                AlertConfig(
+                    alertConfig.delay,
+                    alertConfig.severity,
+                    alertConfig.connection,
+                    alertConfig.enabled
+                )
+            )
+        )
+
+        return setOf(alerts.generateAuroraResource())
+    }
+
+    private fun AuroraDeploymentSpec.extractConfiguration(alertName: String): AlertConfiguration {
+        val confPath = "$featureName/$alertName"
+
+        val alertEnabled = this.getOrNull<Boolean>("$confPath/enabled")
+        val alertExpr = this.getOrNull<String>("$confPath/expr")
+        val alertDelay = this.getOrNull<Int>("$confPath/delay")
+        val alertConnection = this.getOrNull<String>("$confPath/connection")
+        val alertSeverity = this.getOrNull<String>("$confPath/severity")
+
+        if (this.hasSubKeys(defaultsName)) {
+            val defaultEnabled = this.getOrNull<Boolean>("$defaultsName/enabled") ?: false
+            val defaultDelay = this.getOrNull<Int>("$defaultsName/delay") ?: -1
+            val defaultConnection = this.getOrNull<String>("$defaultsName/connection") ?: ""
+
+            return AlertConfiguration(
+                enabled = alertEnabled ?: defaultEnabled,
+                expr = alertExpr ?: "",
+                delay = alertDelay ?: defaultDelay,
+                connection = alertConnection ?: defaultConnection,
+                severity = alertSeverity ?: ""
+            )
+        }
+
+        return AlertConfiguration(
+            alertEnabled ?: false,
+            alertExpr ?: "",
+            alertDelay ?: -1,
+            alertConnection ?: "",
+            alertSeverity ?: ""
+        )
+    }
+}
---
+++ src/test/kotlin/no/skatteetaten/aurora/boober/feature/AlertsFeatureTest.kt
@@ -0,0 +1,157 @@
+package no.skatteetaten.aurora.boober.feature
+
+import assertk.assertThat
+import assertk.assertions.hasSize
+import assertk.assertions.isEmpty
+import assertk.assertions.isEqualTo
+import assertk.assertions.isTrue
+import no.skatteetaten.aurora.boober.model.openshift.Alerts
+import no.skatteetaten.aurora.boober.utils.AbstractFeatureTest
+import no.skatteetaten.aurora.boober.utils.applicationErrors
+import no.skatteetaten.aurora.boober.utils.singleApplicationError
+import org.junit.jupiter.api.Test
+
+class AlertsFeatureTest : AbstractFeatureTest() {
+    override val feature: Feature
+        get() = AlertsFeature()
+
+    @Test
+    fun `should fail validation when expression is missing`() {
+        assertThat {
+            createAuroraDeploymentContext(
+                """{
+                  "alerts": {
+                    "is-down": {
+                      "enabled": true,
+                      "delay": 1,
+                      "connection": "aurora-mattermost",
+                      "severity": "warning"
+                    }
+                  }
+                }"""
+            )
+        }.singleApplicationError(AlertsFeature.Errors.MissingAlertExpression.message)
+    }
+
+    @Test
+    fun `should fail validation when missing configuration properties`() {
+        assertThat {
+            createAuroraDeploymentContext(
+                """{
+                      "alerts": {
+                        "is-down": {
+                          "enabled": true
+                        }
+                      }
+                    }"""
+            )
+        }.applicationErrors(
+            listOf(
+                AlertsFeature.Errors.MissingAlertExpression.message,
+                AlertsFeature.Errors.MissingAlertConnectionProp.message,
+                AlertsFeature.Errors.MissingAlertDelayProp.message,
+                AlertsFeature.Errors.MissingAlertSeverity.message
+            )
+        )
+    }
+
+    @Test
+    fun `should not generate alert resource if enabled is false`() {
+        val generatedResource = generateResources(
+            """{
+              "alerts": {
+                "isdown": {
+                  "enabled": false,
+                  "expr": "ac",
+                  "delay": 1,
+                  "connection": "aurora-mattermost",
+                  "severity": "warning"
+                }
+              }
+            }"""
+        )
+
+        assertThat(generatedResource).isEmpty()
+    }
+
+    @Test
+    fun `should generate alert resource where enabled is when multiple configurations`() {
+        val generatedResources = generateResources(
+            """{
+              "alerts": {
+                "isdown": {
+                  "enabled": false,
+                  "expr": "ac",
+                  "delay": 1,
+                  "connection": "aurora-mattermost",
+                  "severity": "warning"
+                },
+                "halted": {
+                  "enabled": true,
+                  "expr": "av",
+                  "delay": 1,
+                  "connection": "aurora-mattermost",
+                  "severity": "warning"
+                }
+              }
+            }"""
+        )
+
+        assertThat(generatedResources).hasSize(1)
+        val alertSpec = (generatedResources[0].resource as Alerts).spec
+        assertThat(alertSpec.prometheus.expr).isEqualTo("av")
+        assertThat(alertSpec.alert.enabled).isTrue()
+    }
+
+    @Test
+    fun `should use configuration from alertsDefaults if defined for missing configuration values`() {
+        val generatedResources = generateResources(
+            """{
+              "alertsDefaults": {
+                "enabled": true,
+                "connection": "koblingsregel",
+                "delay": 4
+              },
+              "alerts": {
+                "my-alert": {
+                  "expr": "en_metrikk_query('targetLabel') > 10",
+                  "severity": "critical"
+                }
+              }
+            }"""
+        )
+
+        assertThat(generatedResources).hasSize(1)
+        val alert = (generatedResources[0].resource as Alerts).spec.alert
+        assertThat(alert.delay).isEqualTo(4)
+        assertThat(alert.connection).isEqualTo("koblingsregel")
+        assertThat(alert.severity).isEqualTo("critical")
+    }
+
+    @Test
+    fun `should override alertsDefaults configuration if property is defined in alert configuration`() {
+        val generatedResources = generateResources(
+            """{
+              "alertsDefaults": {
+                "enabled": false,
+                "connection": "koblingsregel",
+                "delay": 4
+              },
+              "alerts": {
+                "my-alert": {
+                  "enabled": true,
+                  "expr": "en_metrikk_query('targetLabel') > 10",
+                  "severity": "critical",
+                  "connection": "en-annen-koblingsregel",
+                  "delay": 2
+                }
+              }
+            }"""
+        )
+
+        assertThat(generatedResources).hasSize(1)
+        val alert = (generatedResources[0].resource as Alerts).spec.alert
+        assertThat(alert.delay).isEqualTo(2)
+        assertThat(alert.connection).isEqualTo("en-annen-koblingsregel")
+    }
+}
