ccdbd21e1c900b265cdab269bda0de77acf82fd3
furode <88827279+furode@users.noreply.github.com>
2021-09-30 15:26:14 +0200

    Feature/aos 5327 oppsett av alarmer (#470)
    * AOS-5327: implementerer generering av CRD for alerts
    
    * AOS-5327: fjerner if-sjekk og kaster exception i cases som skal være håndtert av validering
    
    * AOS-5327: oppdaterer AlertsCRD med nye felter og datatype
    
    * AOS-5327: disable test
    
    * AOS-5327: disable test
    
    * AOS-5327: oppdaterer test
    
    * AOS-5327: oppdaterer test
    
    * AOS-5327: oppdaterer test
    
    * AOS-5327: oppdaterer spec-filer
    
    * AOS-5327: fjerner int validering fra delay

---
+++ src/main/kotlin/no/skatteetaten/aurora/boober/model/openshift/Alerts.kt
@@ -0,0 +1,67 @@
+package no.skatteetaten.aurora.boober.model.openshift
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
+import io.fabric8.kubernetes.api.model.HasMetadata
+import io.fabric8.kubernetes.api.model.ObjectMeta
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder(value = ["apiVersion", "kind", "metadata", "spec"])
+data class Alerts(
+    val spec: AlertSpec,
+    @JsonIgnore
+    var _metadata: ObjectMeta?,
+    @JsonIgnore
+    val _kind: String = "Alert",
+    @JsonIgnore
+    var _apiVersion: String = "skatteetaten.no/v1"
+) : HasMetadata {
+    override fun getMetadata(): ObjectMeta {
+        return _metadata ?: ObjectMeta()
+    }
+
+    override fun getKind(): String {
+        return _kind
+    }
+
+    override fun getApiVersion(): String {
+        return _apiVersion
+    }
+
+    override fun setMetadata(metadata: ObjectMeta?) {
+        _metadata = metadata
+    }
+
+    override fun setApiVersion(version: String?) {
+        _apiVersion = apiVersion
+    }
+}
+
+data class AlertSpec(
+    val application: ApplicationConfig,
+    val prometheus: PrometheusConfig,
+    val alert: AlertConfig
+)
+
+data class ApplicationConfig(
+    val affiliation: String,
+    val cluster: String,
+    val environment: String,
+    val name: String
+)
+
+data class PrometheusConfig(
+    val expr: String
+)
+
+data class AlertConfig(
+    val delay: String,
+    val severity: String,
+    val connection: String,
+    val enabled: Boolean,
+    val summary: String,
+    val description: String
+)
