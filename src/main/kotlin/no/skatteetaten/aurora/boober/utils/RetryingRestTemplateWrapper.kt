e36827488b8d14e6328dc8830e0d1e5c5e77c7ea
Rune Offerdal <Rune.Offerdal@skatteetaten.no>
2021-12-03 16:13:43 +0100

    AOS-6006 fix token snippet for sha256

--- src/main/kotlin/no/skatteetaten/aurora/boober/utils/RetryingRestTemplateWrapper.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/utils/RetryingRestTemplateWrapper.kt
@@ -189,6 +189,11 @@ class RetryLogger(val logger: Logger) : RetryListenerSupport() {
             headers.get(HttpHeaders.AUTHORIZATION)
                 ?.firstOrNull()
                 ?.split(" ")
-                ?.get(1)?.let { it.substring(0, Integer.min(it.length, 5)) }
+                ?.get(1)?.let {
+                    if (it.startsWith("sha256") && it.length >= 11)
+                        it.substring(6, 11)
+                    else
+                        it.substring(0, Integer.min(it.length, 5))
+                }
     }
 }
