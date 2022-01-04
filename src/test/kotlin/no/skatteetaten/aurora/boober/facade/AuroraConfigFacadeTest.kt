0819a4513a1fad9f2e399639e2cc1746cbc77e95
Martin Rodland <Martin.Rodland@skatteetaten.no>
2022-01-04 10:23:55 +0100

    disabled dertiestContext for AuroraConfigFacadeTest and DeploymentFacadeTest

--- src/test/kotlin/no/skatteetaten/aurora/boober/facade/AuroraConfigFacadeTest.kt
+++ src/test/kotlin/no/skatteetaten/aurora/boober/facade/AuroraConfigFacadeTest.kt
@@ -29,12 +29,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.test.annotation.DirtiesContext
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.NONE
 )
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+// @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @TestInstance(TestInstance.Lifecycle.PER_METHOD)
 class AuroraConfigFacadeTest(
     @Value("\${application.deployment.id}") val booberAdId: String
