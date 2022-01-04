4ffbbd9c29e11f6f63a6f43c24dc692d9b6fd787
Kristoffer Moberg Christensen <kristoffermoberg.christensen@skatteetaten.no>
2022-01-04 13:58:14 +0100

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("no.skatteetaten.gradle.aurora") version "4.3.24"
+    id("no.skatteetaten.gradle.aurora") version "4.4.9"
 }
 
 aurora {
@@ -9,17 +9,17 @@ aurora {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.5.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 
-    implementation("org.eclipse.jgit:org.eclipse.jgit:5.13.0.202109080827-r")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:6.0.0.202111291000-r")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
     implementation("com.google.guava:guava:31.0.1-jre")
     implementation("org.encryptor4j:encryptor4j:0.1.2")
     // The above library uses an vulnerable bcprov, set the fixed version here, hopefully this will work.
     // pr is sent to maintainer
-    implementation("org.bouncycastle:bcprov-jdk15on:1.69")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.0.4")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.70")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.0.5")
     implementation("org.apache.commons:commons-text:1.9")
 
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
@@ -38,9 +38,9 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 
-    testImplementation("io.mockk:mockk:1.12.0")
+    testImplementation("io.mockk:mockk:1.12.2")
     testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
     testImplementation("no.skatteetaten.aurora:mockmvc-extensions-kotlin:1.1.7")
     testImplementation("no.skatteetaten.aurora:mockwebserver-extensions-kotlin:1.2.0")
-    testImplementation("com.ninja-squad:springmockk:3.0.1")
+    testImplementation("com.ninja-squad:springmockk:3.1.0")
 }
