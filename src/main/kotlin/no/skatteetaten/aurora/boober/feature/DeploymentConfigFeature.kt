91c94ae378a0e6292ba80e1075663aeff2ee4d1f
Espen Strømland <espen.stromland@skatteetaten.no>
2022-05-11 10:25:28 +0200

    fix: changed format for AURORA_KLIENTID
    Some systems were not compliant with the new format, so we revert to the old format.

--- src/main/kotlin/no/skatteetaten/aurora/boober/feature/DeploymentConfigFeature.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/feature/DeploymentConfigFeature.kt
@@ -206,8 +206,7 @@ class DeploymentConfigFeature() : Feature {
     }
 
     private fun createAuroraKlientId(adc: AuroraDeploymentSpec): String {
-        val segment: String? = adc.getOrNull("segment")
         // APP_VERSION is available for all images created by Architect
-        return "${segment ?: adc.affiliation}/${adc.artifactId}/\${APP_VERSION}"
+        return "${adc.name}/\${APP_VERSION}"
     }
 }
