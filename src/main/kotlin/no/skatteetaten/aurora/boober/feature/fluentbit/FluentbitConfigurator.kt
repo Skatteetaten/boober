b64bd4507524e5ac7c12ba5b26f6d49aba4b0bda
Engebretsen, Tomas <Tomas.Engebretsen@skatteetaten.no>
2022-05-20 10:32:43 +0200

    Legger til evalXmlTimeParser

--- src/main/kotlin/no/skatteetaten/aurora/boober/feature/fluentbit/FluentbitConfigurator.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/feature/fluentbit/FluentbitConfigurator.kt
@@ -10,28 +10,35 @@ class FluentbitConfigurator {
          *   It uses the timestamp to recognize the first line of a log line and continues until it meets another timestamp
          */
         fun parserConf(): String = """
-        |[PARSER]
-        |   Name        timeParser
-        |   Format      regex
-        |   Regex       ^(?<timestamp>\d{4}-\d{1,2}-\d{1,2}T\d{2}:\d{2}:\d{2},\d*Z) (.*)
-        |   Time_Key    timestamp
-        |   Time_Format %Y-%m-%dT%H:%M:%S,%L%z
-        |
-        |[MULTILINE_PARSER]
-        |   name          multiline-log4j
-        |   type          regex
-        |   key_content   event
-        |   flush_timeout 1000
-        |   rule          "start_state"   "/^(\d{4}-\d{1,2}-\d{1,2}T\d{2}:\d{2}:\d{2},\d*(Z|\+\d{4}))(.*)$/"  "cont"
-        |   rule          "cont"          "/^(?!\d{4}-\d{1,2}-\d{1,2}T\d{2}:\d{2}:\d{2},\d*(Z|\+\d{4}))(.*)$/"  "cont"
-        |
-        |[MULTILINE_PARSER]
-        |   name          multiline-evalevent-xml
-        |   type          regex
-        |   key_content   event
-        |   flush_timeout 1000
-        |   rule          "start_state" "/<EvaluationEvent.*/"       "cont"
-        |   rule          "cont"        "/^(?!<EvaluationEvent).*$/" "cont"
+            |[PARSER]
+            |   Name        timeParser
+            |   Format      regex
+            |   Regex       ^(?<timestamp>\d{4}-\d{1,2}-\d{1,2}T\d{2}:\d{2}:\d{2},\d*Z) (.*)
+            |   Time_Key    timestamp
+            |   Time_Format %Y-%m-%dT%H:%M:%S,%L%z
+            |
+            |[MULTILINE_PARSER]
+            |   name          multiline-log4j
+            |   type          regex
+            |   key_content   event
+            |   flush_timeout 1000
+            |   rule          "start_state"   "/^(\d{4}-\d{1,2}-\d{1,2}T\d{2}:\d{2}:\d{2},\d*(Z|\+\d{4}))(.*)$/"  "cont"
+            |   rule          "cont"          "/^(?!\d{4}-\d{1,2}-\d{1,2}T\d{2}:\d{2}:\d{2},\d*(Z|\+\d{4}))(.*)$/"  "cont"
+            |
+            |[PARSER]
+            |   Name        evalXmlTimeParser
+            |   Format      regex
+            |   Regex       (?<timestamp>(?<=<Timestamp>)\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d\.\d+Z(?=<\/Timestamp>))
+            |   Time_Key    timestamp
+            |   Time_Format %FT%T.%L%z
+            |
+            |[MULTILINE_PARSER]
+            |   name          multiline-eval-xml
+            |   type          regex
+            |   key_content   event
+            |   flush_timeout 1000
+            |   rule          "start_state" "/<EvaluationEvent.*/"       "cont"
+            |   rule          "cont"        "/^(?!<EvaluationEvent).*$/" "cont"
         """.trimMargin()
 
         /**
@@ -69,6 +76,7 @@ class FluentbitConfigurator {
                 fluentbitLogInputAndFilter,
                 timeParserFilter,
                 multilineLog4jFilter,
+                evalXmlTimeParserFilter,
                 multilineEvalXmlFilter,
                 getModifyFilter(application, cluster, version),
                 applicationSplunkOutputs,
@@ -81,12 +89,12 @@ class FluentbitConfigurator {
         }
 
         private val fluentbitService: String = """
-        |[SERVICE]
-        |   Flush        1
-        |   Daemon       Off
-        |   Log_Level    info
-        |   Log_File     /u01/logs/fluentbit
-        |   Parsers_File $parserMountPath/$parsersFileName
+            |[SERVICE]
+            |   Flush        1
+            |   Daemon       Off
+            |   Log_Level    info
+            |   Log_File     /u01/logs/fluentbit
+            |   Parsers_File $parserMountPath/$parsersFileName
         """.trimMargin()
 
         // Fluentibt input for each logging config
@@ -95,85 +103,103 @@ class FluentbitConfigurator {
             bufferSize: Int
         ) = loggerIndexes.joinToString("\n\n") { log ->
             """
-            |[INPUT]
-            |   Name            tail
-            |   Path            /u01/logs/${log.filePattern}
-            |   Path_Key        source
-            |   Exclude_Path    ${log.excludePattern}
-            |   Read_From_Head  true
-            |   Tag             ${log.name}-${log.sourceType}
-            |   DB              /u01/logs/${log.name}.db
-            |   Buffer_Max_Size 512k
-            |   Skip_Long_Lines On
-            |   Mem_Buf_Limit   ${bufferSize}MB
-            |   Rotate_Wait     10
-            |   Key             event
+                |[INPUT]
+                |   Name            tail
+                |   Path            /u01/logs/${log.filePattern}
+                |   Path_Key        source
+                |   Exclude_Path    ${log.excludePattern}
+                |   Read_From_Head  true
+                |   Tag             ${log.name}-${log.sourceType}
+                |   DB              /u01/logs/${log.name}.db
+                |   Buffer_Max_Size 512k
+                |   Skip_Long_Lines On
+                |   Mem_Buf_Limit   ${bufferSize}MB
+                |   Rotate_Wait     10
+                |   Key             event
             """.trimMargin()
         }
 
         // Input for the log file produced by fluentbit
         // Filters it to stdout
         private val fluentbitLogInputAndFilter: String = """
-        |[INPUT]
-        |   Name             tail
-        |   Path             /u01/logs/fluentbit
-        |   Path_Key         source
-        |   Tag              fluentbit
-        |   Refresh_Interval 5
-        |   Read_from_Head   true
-        |   Key              event
-        |
-        |[FILTER]
-        |   Name             stdout
-        |   Match            fluentbit
+            |[INPUT]
+            |   Name             tail
+            |   Path             /u01/logs/fluentbit
+            |   Path_Key         source
+            |   Tag              fluentbit
+            |   Refresh_Interval 5
+            |   Read_from_Head   true
+            |   Key              event
+            |
+            |[FILTER]
+            |   Name             stdout
+            |   Match            fluentbit
         """.trimMargin()
 
         // Parser filter to assign it to application tag records
         private val timeParserFilter = """
-        |[FILTER]
-        |   Name parser
-        |   Match *-log4j
-        |   Key_Name event
-        |   Parser timeParser
-        |   Preserve_Key On
-        |   Reserve_Data On
+            |[FILTER]
+            |   Name parser
+            |   Match *-log4j
+            |   Key_Name event
+            |   Parser timeParser
+            |   Preserve_Key On
+            |   Reserve_Data On
         """.trimMargin()
 
         // Multiline filter to assign the multiline_parser to application tag records
         private val multilineLog4jFilter = """
-        |[FILTER]
-        |   name multiline
-        |   match *-log4j
-        |   multiline.key_content event
-        |   multiline.parser multiline-log4j
+            |[FILTER]
+            |   name multiline
+            |   match *-log4j
+            |   multiline.key_content event
+            |   multiline.parser multiline-log4j
+        """.trimMargin()
+
+        private val evalXmlTimeParserFilter = """
+            |[FILTER]
+            |   Name parser
+            |   Match *-evalevent_xml
+            |   Key_Name event
+            |   Parser evalXmlTimeParser
+            |   Preserve_Key On
+            |   Reserve_Data On
+            |
+            |[FILTER]
+            |   Name parser
+            |   Match *-ats:eval:xml
+            |   Key_Name event
+            |   Parser evalXmlTimeParser
+            |   Preserve_Key On
+            |   Reserve_Data On
         """.trimMargin()
 
         // Multiline filter to assign the XML multiline parser to application tag records
         private val multilineEvalXmlFilter = """
-        |[FILTER]
-        |   name multiline
-        |   match *-evalevent_xml
-        |   multiline.key_content event
-        |   multiline.parser multiline-evalevent-xml
-        |
-        |[FILTER]
-        |   name multiline
-        |   match *-ats:eval:xml
-        |   multiline.key_content event
-        |   multiline.parser multiline-evalevent-xml
+            |[FILTER]
+            |   name multiline
+            |   match *-evalevent_xml
+            |   multiline.key_content event
+            |   multiline.parser multiline-eval-xml
+            |
+            |[FILTER]
+            |   name multiline
+            |   match *-ats:eval:xml
+            |   multiline.key_content event
+            |   multiline.parser multiline-eval-xml
         """.trimMargin()
 
         // Fluentbit filter for adding splunk fields for application, cluster, environment, host and nodetype to the record
         private fun getModifyFilter(application: String, cluster: String, version: String) = """
-        |[FILTER]
-        |   Name  modify
-        |   Match *
-        |   Add   host $ {POD_NAME}
-        |   Add   environment $ {POD_NAMESPACE}
-        |   Add   version $version
-        |   Add   nodetype openshift
-        |   Add   name $application
-        |   Add   cluster $cluster
+            |[FILTER]
+            |   Name  modify
+            |   Match *
+            |   Add   host $ {POD_NAME}
+            |   Add   environment $ {POD_NAMESPACE}
+            |   Add   version $version
+            |   Add   nodetype openshift
+            |   Add   name $application
+            |   Add   cluster $cluster
         """.trimMargin()
 
         /**
@@ -189,31 +215,31 @@ class FluentbitConfigurator {
         ): String {
             val retryConfigOrEmpty = retryLimit?.let {
                 """
-                |
-                |   Retry_Limit $it
+                    |
+                    |   Retry_Limit $it
                 """.trimMargin()
             } ?: ""
 
             return """
-            |[OUTPUT]
-            |   Name                       splunk
-            |   Match                      $matcherTag 
-            |   Host                       $ {SPLUNK_HOST}
-            |   Port                       $ {SPLUNK_PORT}
-            |   Splunk_token               $ {HEC_TOKEN}
-            |   TLS                        On
-            |   TLS.Verify                 Off
-            |   event_index                $index
-            |   event_sourcetype           $sourceType
-            |   event_host                 $ {POD_NAME}
-            |   event_source               ${'$'}source
-            |   event_field                application ${'$'}name
-            |   event_field                cluster ${'$'}cluster
-            |   event_field                environment ${'$'}environment
-            |   event_field                nodetype ${'$'}nodetype
-            |   event_field                version ${'$'}version
-            |   event_key                  ${'$'}event
-            |   net.keepalive_idle_timeout 10
+                |[OUTPUT]
+                |   Name                       splunk
+                |   Match                      $matcherTag 
+                |   Host                       $ {SPLUNK_HOST}
+                |   Port                       $ {SPLUNK_PORT}
+                |   Splunk_token               $ {HEC_TOKEN}
+                |   TLS                        On
+                |   TLS.Verify                 Off
+                |   event_index                $index
+                |   event_sourcetype           $sourceType
+                |   event_host                 $ {POD_NAME}
+                |   event_source               ${'$'}source
+                |   event_field                application ${'$'}name
+                |   event_field                cluster ${'$'}cluster
+                |   event_field                environment ${'$'}environment
+                |   event_field                nodetype ${'$'}nodetype
+                |   event_field                version ${'$'}version
+                |   event_key                  ${'$'}event
+                |   net.keepalive_idle_timeout 10
             """.trimMargin() + retryConfigOrEmpty
         }
     }
