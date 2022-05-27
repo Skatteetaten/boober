201577dfe1b7a00b8985469dd8ed4106cbb7bbef
Frode Mathiesen <Frode.Mathiesen@skatteetaten.no>
2022-05-27 13:14:30 +0200

    add comment

--- src/main/kotlin/no/skatteetaten/aurora/boober/feature/BigIpFeature.kt
+++ src/main/kotlin/no/skatteetaten/aurora/boober/feature/BigIpFeature.kt
@@ -105,12 +105,14 @@ class BigIpFeature(
                 val hasApiPaths = !adc.getDelimitedStringOrArrayAsSetOrNull("bigip/$it/apiPaths").isNullOrEmpty()
                 val missingExternalHost = adc.getOrNull<String>("bigip/$it/externalHost").isNullOrEmpty()
                 val isEnabled = adc.isBigIPHostEnabled(it)
+                // check if enabled to prevent breaking existing configuration where enabled=false
                 isEnabled && hasApiPaths && missingExternalHost
             }
         } else {
             val hasApiPaths = !adc.getDelimitedStringOrArrayAsSetOrNull("bigip/apiPaths").isNullOrEmpty()
             val missingExternalHost = adc.getOrNull<String>("bigip/externalHost").isNullOrEmpty()
             val isEnabled = adc.isLegacyFeatureEnabled()
+            // check if enabled to prevent breaking existing configuration where enabled=false
             isEnabled && hasApiPaths && missingExternalHost
         }
 
